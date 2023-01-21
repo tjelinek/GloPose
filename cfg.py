bc7494e20d03b85ac416892e085d846a1176497a
tjelinek <tomasjelinek96@gmail.com>
2023-01-21 11:28:10 +0100

    added track6d namespace

--- cfg.py
+++ cfg.py
@@ -1,8 +1,8 @@
 import sys
 from pathlib import Path
 
-sys.path.append('')
 sys.path.append('GMA/core')
+sys.path.append('track6d')
 
 FLOW_OUT_DEFAULT_DIR = Path('./data/flow_out')
 DEVICE = 'cuda'
