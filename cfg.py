2b7a98caacf140a477cd9b35dfaee20e31c80d00
tjelinek <tomasjelinek96@gmail.com>
2023-01-21 09:49:36 +0100

    changed default flow out folder

--- cfg.py
+++ cfg.py
@@ -4,5 +4,5 @@ from pathlib import Path
 sys.path.append('')
 sys.path.append('GMA/core')
 
-FLOW_OUT_DEFAULT_DIR = Path('../data/flow_out')
+FLOW_OUT_DEFAULT_DIR = Path('/data/flow_out')
 DEVICE = 'cuda'
