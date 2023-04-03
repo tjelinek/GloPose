cf782374cfd668bd9c9c6b5784f47e7919b8a346
tjelinek <tomasjelinek96@gmail.com>
2023-04-03 13:57:47 +0200

    minor error

--- denys_download_datasets.sh
+++ denys_download_datasets.sh
@@ -1,6 +1,6 @@
 FOLDER=/cluster/scratch/denysr/dataset/
 WEIGHTS=/cluster/home/denysr/scratch/tmp/
-scp jelint19@ptak.felk.cvut.cz:/datagrid/personal/jelint19/360photo/360photo.zip $FOLDER/360photo/
+scp jelint19@ptak.felk.cvut.cz:/datagrid/personal/rozumden/360photo/360photo.zip $FOLDER/360photo/
 unzip $FOLDER/360photo/360photo.zip -d $FOLDER/360photo/
 
 scp jelint19@ptak.felk.cvut.cz:/mnt/datasets/votrgbd/votrgbd.zip $FOLDER/votrgbd/
