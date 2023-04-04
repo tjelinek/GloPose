36a098b971ae88113fe486970bbb753822e8d71d
tjelinek <tomasjelinek96@gmail.com>
2023-04-04 09:21:09 +0200

    tracker wrapper

--- tracker_wrapper.py
+++ tracker_wrapper.py
@@ -4,7 +4,8 @@ if __name__ == '__main__':
     args = sys.argv
 
     if len(args) > 1:
-        file_name = args[1]
+        folders_path = args[1]
+        bboxes_paths = args[1]
     else:
         raise AssertionError("Expected the folder with sequence as an input")
 
