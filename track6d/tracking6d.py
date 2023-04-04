526844555079eed7e06d32ede71f07ca53762808
tjelinek <tomasjelinek96@gmail.com>
2023-04-04 11:35:21 +0200

    import optimization

--- track6d/tracking6d.py
+++ track6d/tracking6d.py
@@ -12,7 +12,6 @@ from torchvision.utils import save_image
 from S2DNet.s2dnet import S2DNet
 from helpers.torch_helpers import write_renders
 from main_settings import g_ext_folder
-print(os.getcwd())
 from models.encoder import Encoder, qmult, qnorm
 from models.initial_mesh import generate_initial_mesh, generate_face_features
 from models.kaolin_wrapper import load_obj, write_obj_mesh
