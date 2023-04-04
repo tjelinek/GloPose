623816335c228627c72778c1d1c6d3720acdd500
tjelinek <tomasjelinek96@gmail.com>
2023-04-04 11:07:35 +0200

    slightly altered

--- track6d/run_360.py
+++ track6d/run_360.py
@@ -1,8 +1,9 @@
 import os
 import sys
-sys.path.append('./OSTrack')
-sys.path.append('./OSTrack/lib')
-sys.path.append('./OSTrack/S2DNet')
+# sys.path.append('./OSTrack')
+# sys.path.append('./OSTrack/lib')
+# sys.path.append('./OSTrack/S2DNet')
+sys.path.insert(0, './OSTrack')
 import time
 
 import numpy as np
