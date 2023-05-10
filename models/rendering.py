fixed twice backprop bug

models/rendering.py
@@ -195,18 +195,16 @@ class RenderingKaolin(nn.Module):
         # Extract the z-components of the face normals
         face_normals_z = face_normals[:, :, -1]
 
+        features_for_rendering = torch.cat((face_features, face_vertices_cam), dim=-1)
+
         # Perform dibr rasterization
         ren_outputs, ren_mask, red_index = kaolin.render.mesh.dibr_rasterization(self.height, self.width,
                                                                                  face_vertices_z,
                                                                                  face_vertices_image,
-                                                                                 torch.cat(
-                                                                                     (face_features, face_vertices_cam),
-                                                                                     dim=-1),
+                                                                                 features_for_rendering,
                                                                                  face_normals_z,
                                                                                  sigmainv=self.config.sigmainv,
-                                                                                 boxlen=0.02,
-                                                                                 knum=30,
-                                                                                 multiplier=1000)
+                                                                                 boxlen=0.02, knum=30, multiplier=1000)
 
         # Extract ren_mesh_vertices_features and ren_mesh_vertices_coords from the combined output tensor
         ren_mesh_vertices_features = ren_outputs[..., :face_features.shape[-1]]

tracking6d.py
@@ -99,7 +99,7 @@ class TrackerConfig:
     loss_dist_weight: float = 0.0
     loss_qt_weight: float = 0.0
     loss_rgb_weight: float = 0.0
-    loss_flow_weight: float = 1.0
+    loss_flow_weight: float = 0
 
     # Additional settings
     sigmainv: float = None
@@ -387,10 +387,8 @@ class Tracking6D:
     def write_results(self, all_input, all_proj, all_proj_filtered, all_segm, b0, baseline_iou, bboxes, our_iou,
                       our_losses, segment, silh_losses, stepi, observed_flow):
         if self.config.features == 'deep':
-            self.rgb_apply(self.images[:, :, :, b0[0]:b0[1], b0[2]:b0[3]],
-                           self.segments[:, :, :, b0[0]:b0[1], b0[2]:b0[3]], observed_flow, self.keyframes)
-
             tex = nn.Sigmoid()(self.rgb_encoder.texture_map)
+            
         with torch.no_grad():
             translation, quaternion, vertices, texture_maps, lights, tdiff, qdiff = self.encoder(self.keyframes)
 