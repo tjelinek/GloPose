c2747b2a8f19309cbdfbf31e4bcc9aea286f7cc4
Tomas Jelinek <tomasjelinek96@gmail.com>
2023-06-13 11:52:28 +0200

    going back, new flow loss was causing divergence

--- models/loss.py
+++ models/loss.py
@@ -74,8 +74,8 @@ class FMOLoss(nn.Module):
             losses["tv"] = losses["tv"].sum(dim=1)
 
         if self.config.loss_flow_weight > 0:
-            observed_flow *= flow_segment_masks
-            observed_flow = observed_flow.permute(0, 1, 3, 4, 2)
+            observed_flow = observed_flow * flow_segment_masks
+            observed_flow = observed_flow[:, -2:-1].permute(0, 1, 3, 4, 2)
 
             observed_flow[..., 0] *= observed_flow.shape[-2] * 0.5
             observed_flow[..., 1] *= observed_flow.shape[-3] * 0.5
