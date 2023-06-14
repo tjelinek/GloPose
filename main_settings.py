1105a4204524776ba8540cdffb693241bb8bc58f
Tomas Jelinek <tomasjelinek96@gmail.com>
2023-06-14 15:13:00 +0200

    changed default data folder

--- main_settings.py
+++ main_settings.py
@@ -10,7 +10,7 @@ if not os.path.exists(dataset_folder):
 
 # TODO: put your paths here:
 if not os.path.exists(dataset_folder):
-	dataset_folder = str(Path('~/data/').expanduser())
+	dataset_folder = str(Path('/mnt/personal/jelint19/data/').expanduser())
 	tmp_folder = str(Path('/mnt/personal/jelint19/results/FlowTracker/').expanduser())
 
 g_tbd_folder = dataset_folder+'TbD/'
