06339ec52e1ec7e74f20fe1deef50fe8cd0f13e3
Tomas Jelinek <tomasjelinek96@gmail.com>
2023-06-21 13:09:18 +0200

    removed duplicates, optimized imports

--- utils.py
+++ utils.py
@@ -3,15 +3,12 @@ import math
 import numpy as np
 import torch
 import yaml
-from kornia.geometry.conversions import quaternion_to_rotation_matrix, QuaternionCoeffOrder
-from scipy import signal
 from scipy.ndimage import center_of_mass
-from skimage.draw import line_aa
 from skimage.measure import label, regionprops
 from torch import Tensor
 from typing import Tuple
 
-from main_settings import *
+from main_settings import tmp_folder
 
 
 def segment2bbox(segment):
@@ -43,14 +40,14 @@ def load_config(config_name):
 
 
 def fmo_detect(I, B):
-    ## simulate FMO detector -> find approximate location of FMO
+    # simulate FMO detector -> find approximate location of FMO
     dI = (np.sum(np.abs(I - B), 2) > 0.05).astype(float)
     labeled = label(dI)
     regions = regionprops(labeled)
     ind = -1
     maxsol = 0
     for ki in range(len(regions)):
-        if regions[ki].area > 100 and regions[ki].area < 0.01 * np.prod(dI.shape):
+        if 100 < regions[ki].area < 0.01 * np.prod(dI.shape):
             if regions[ki].solidity > maxsol:
                 ind = ki
                 maxsol = regions[ki].solidity
@@ -79,7 +76,6 @@ def imwrite(im, name=tmp_folder + 'tmp.png'):
 
 
 def fmo_detect_maxarea(I, B):
-    ## simulate FMO detector -> find approximate location of FMO
     dI = (np.sum(np.abs(I - B), 2) > 0.1).astype(float)
     labeled = label(dI)
     regions = regionprops(labeled)
@@ -93,60 +89,6 @@ def fmo_detect_maxarea(I, B):
         return [], 0
     bbox = np.array(regions[ind].bbox).astype(int)
     return bbox, regions[ind].minor_axis_length
-
-
-def fmo_detect_hs(gt_hs, B):
-    dI = (np.sum((np.sum(np.abs(gt_hs - B[:, :, :, None]), 2) > 0.1), 2) > 0.5).astype(float)
-    labeled = label(dI)
-    regions = regionprops(labeled)
-    ind = -1
-    maxarea = 0
-    for ki in range(len(regions)):
-        if regions[ki].area > maxarea:
-            ind = ki
-            maxarea = regions[ki].area
-    if ind == -1:
-        return [], 0
-    bbox = np.array(regions[ind].bbox).astype(int)
-    return bbox, regions[ind].minor_axis_length
-
-
-def bbox_detect_hs(gt_hs, B):
-    dI = (np.sum(np.abs(gt_hs - B), 2) > 0.1).astype(float)
-    labeled = label(dI)
-    regions = regionprops(labeled)
-    ind = -1
-    maxarea = 0
-    for ki in range(len(regions)):
-        if regions[ki].area > maxarea:
-            ind = ki
-            maxarea = regions[ki].area
-    if ind == -1:
-        return []
-    bbox = np.array(regions[ind].bbox).astype(int)
-    return bbox
-
-
-def fmo_model(B, H, F, M):
-    if len(H.shape) == 2:
-        H = H[:, :, np.newaxis]
-        F = F[:, :, :, np.newaxis]
-    elif len(F.shape) == 3:
-        F = np.repeat(F[:, :, :, np.newaxis], H.shape[2], 3)
-    HM3 = np.zeros(B.shape)
-    HF = np.zeros(B.shape)
-    for hi in range(H.shape[2]):
-        M1 = M
-        if len(M.shape) > 2:
-            M1 = M[:, :, hi]
-        M3 = np.repeat(M1[:, :, np.newaxis], 3, axis=2)
-        HM = signal.fftconvolve(H[:, :, hi], M1, mode='same')
-        HM3 += np.repeat(HM[:, :, np.newaxis], 3, axis=2)
-        F3 = F[:, :, :, hi]
-        for kk in range(3):
-            HF[:, :, kk] += signal.fftconvolve(H[:, :, hi], F3[:, :, kk], mode='same')
-    I = B * (1 - HM3) + HF
-    return I
 
 
 def montageF(F):
@@ -225,50 +167,10 @@ def calciou(p1, p2, rad):
 
     theta = 2 * np.arccos(dists / (2 * rad))
     A = ((rad * rad) / 2) * (theta - np.sin(theta))
-    I = 2 * A
-    U = 2 * np.pi * rad * rad - I
-    iou = I / U
+    intersection = 2 * A
+    union = 2 * np.pi * rad * rad - intersection
+    iou = intersection / union
     return iou
-
-
-def write_trajectory(Img, traj):
-    for kk in range(traj.shape[1] - 1):
-        Img = renderTraj(np.c_[traj[:, kk], traj[:, kk + 1] - traj[:, kk]][::-1], Img)
-    # Img[traj[1].astype(int),traj[0].astype(int),1] = 1.0
-    return Img
-
-
-def renderTraj(pars, H):
-    # Input: pars is either 2x2 (line) or 2x3 (parabola)
-    if pars.shape[1] == 2:
-        pars = np.concatenate((pars, np.zeros((2, 1))), 1)
-        ns = 2
-    else:
-        ns = 5
-
-    ns = np.max([2, ns])
-
-    rangeint = np.linspace(0, 1, ns)
-    for timeinst in range(rangeint.shape[0] - 1):
-        ti0 = rangeint[timeinst]
-        ti1 = rangeint[timeinst + 1]
-        start = pars[:, 0] + pars[:, 1] * ti0 + pars[:, 2] * (ti0 * ti0)
-        end = pars[:, 0] + pars[:, 1] * ti1 + pars[:, 2] * (ti1 * ti1)
-        start = np.round(start).astype(np.int32)
-        end = np.round(end).astype(np.int32)
-        rr, cc, val = line_aa(start[0], start[1], end[0], end[1])
-        valid = np.logical_and(np.logical_and(rr < H.shape[0], cc < H.shape[1]), np.logical_and(rr > 0, cc > 0))
-        rr = rr[valid]
-        cc = cc[valid]
-        val = val[valid]
-        if len(H.shape) > 2:
-            H[rr, cc, 0] = 0
-            H[rr, cc, 1] = 0
-            H[rr, cc, 2] = val
-        else:
-            H[rr, cc] = val
-
-    return H
 
 
 def generate_lowFPSvideo(V, k=8, gamma_coef=0.4, do_WB=True):
@@ -283,40 +185,6 @@ def generate_lowFPSvideo(V, k=8, gamma_coef=0.4, do_WB=True):
     return Vk_WB
 
 
-def extend_bbox(bbox, ext, aspect_ratio, shp):
-    height, width = bbox[2] - bbox[0], bbox[3] - bbox[1]
-
-    h2 = height + ext
-
-    h2 = int(np.ceil(np.ceil(h2 / aspect_ratio) * aspect_ratio))
-    w2 = int(h2 / aspect_ratio)
-
-    wdiff = w2 - width
-    wdiff2 = int(np.round(wdiff / 2))
-    hdiff = h2 - height
-    hdiff2 = int(np.round(hdiff / 2))
-
-    bbox[0] -= hdiff2
-    bbox[2] += hdiff - hdiff2
-    bbox[1] -= wdiff2
-    bbox[3] += wdiff - wdiff2
-    bbox[bbox < 0] = 0
-    bbox[2] = np.min([bbox[2], shp[0] - 1])
-    bbox[3] = np.min([bbox[3], shp[1] - 1])
-    return bbox
-
-
-def extend_bbox_uniform(bbox, ext, shp):
-    bbox[0] -= ext
-    bbox[2] += ext
-    bbox[1] -= ext
-    bbox[3] += ext
-    bbox[bbox < 0] = 0
-    bbox[2] = np.min([bbox[2], shp[0] - 1])
-    bbox[3] = np.min([bbox[3], shp[1] - 1])
-    return bbox
-
-
 def extend_bbox_nonuniform(bbox, ext, shp):
     bbox[0] -= ext[0]
     bbox[2] += ext[0]
@@ -328,35 +196,12 @@ def extend_bbox_nonuniform(bbox, ext, shp):
     return bbox
 
 
-def bbox_fmo(bbox, gt_hs, B):
-    gt_hs_crop = crop_only(gt_hs, bbox)
-    B_crop = crop_only(B, bbox)
-    bbox_crop, rad = fmo_detect_hs(gt_hs_crop, B_crop)
-    bbox_new = bbox_crop
-    if len(bbox_new) > 0:
-        bbox_new[:2] += bbox[:2]
-        bbox_new[2:] += bbox[:2]
-    else:
-        bbox_new = bbox
-    return bbox_new
-
-
 def rgba2hs(rgba, bgr):
     return rgba[:, :, :3] * rgba[:, :, 3:] + bgr[:, :, :, None] * (1 - rgba[:, :, 3:])
 
 
 def rgba2rgb(rgba):
     return rgba[:, :, :3] * rgba[:, :, 3:] + 1 * (1 - rgba[:, :, 3:])
-
-
-def sync_directions(est_hs_crop, gt_hs_crop):
-    do_flip = False
-    if gt_hs_crop is not None:
-        if np.mean((est_hs_crop[:, :, :, 0] - gt_hs_crop[:, :, :, 0]) ** 2) > np.mean(
-                (est_hs_crop[:, :, :, 0] - gt_hs_crop[:, :, :, -1]) ** 2):
-            est_hs_crop = est_hs_crop[:, :, :, ::-1]
-            do_flip = True
-    return est_hs_crop, do_flip
 
 
 def sync_directions_smooth(est_hs_crop, est_traj, est_traj_prev, radius):
@@ -390,20 +235,6 @@ def crop_resize(Is, bbox, res):
     if rev_axis:
         imr = imr[:, :, :, 0]
     return imr
-
-
-def crop_only(Is, bbox):
-    if Is is None:
-        return None
-    return Is[bbox[0]:bbox[2], bbox[1]:bbox[3]]
-
-
-def rev_crop_resize_traj(inp, bbox, res):
-    inp[0] *= ((bbox[2] - bbox[0]) / res[1])
-    inp[1] *= ((bbox[3] - bbox[1]) / res[0])
-    inp[0] += bbox[0]
-    inp[1] += bbox[1]
-    return np.array(inp[[1, 0]])
 
 
 def quaternion_from_euler(roll: Tensor, pitch: Tensor, yaw: Tensor) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
@@ -522,15 +353,6 @@ def quaternion_multiply(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     """
     ab = quaternion_raw_multiply(a, b)
     return standardize_quaternion(ab)
-
-
-def rev_crop_resize(inp, bbox, I):
-    est_hs = np.tile(I.copy()[:, :, :, np.newaxis], (1, 1, 1, inp.shape[3]))
-    for hsk in range(inp.shape[3]):
-        est_hs[bbox[0]:bbox[2], bbox[1]:bbox[3], :, hsk] = cv2.resize(inp[:, :, :, hsk],
-                                                                      (bbox[3] - bbox[1], bbox[2] - bbox[0]),
-                                                                      interpolation=cv2.INTER_CUBIC)
-    return est_hs
 
 
 def calculate_rotation_difference(q1, q2):
