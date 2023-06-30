17275844df36e56ad391c570e3988748a1fa45dd
Tomas Jelinek <tomasjelinek96@gmail.com>
2023-06-30 15:03:22 +0200

    changed arguments

--- flow_mft.py
+++ flow_mft.py
@@ -7,16 +7,26 @@ from argparse import Namespace
 from cfg import DEVICE
 from flow import export_flow_from_files
 
-sys.path.append('RAFT-occl-uncertainty/RAFT')
+# sys.path.append('RAFTocclUncertainty/RAFT')
+# sys.path.append('RAFTocclUncertainty')
 
 from RAFT.core.raft import RAFT
 
 
 def get_flow_model_mft():
-    args = Namespace(model='/home/jelint19/datagrid/mnt/home.stud/neoramic/repos/raft_new_debug/RAFT/checkpoints/'
-                           '50000_raft-things-sintel-occlusion-uncertainty.pth', model_name='RAFT', path=None,
+    args = Namespace(model='RAFTocclUncertainty/RAFT/models/'
+                           'raft-things-sintel-kubric-splitted-occlusion-uncertainty-non-occluded-base-sintel.pth',
+                     model_name='RAFT', path=None,
                      mixed_precision=True, alternate_corr=False, small=False)
-    model = torch.nn.DataParallel(RAFT(args))
+    raft_kwargs = {
+        'occlusion_module': 'separate_with_uncertainty',
+        'restore_ckpt': 'RAFTocclUncertainty/RAFT/models',
+        'small': False,
+        'mixed_precision': False,
+    }
+
+    raft_params = Namespace(**raft_kwargs)
+    model = torch.nn.DataParallel(RAFT(raft_params))
     model.load_state_dict(torch.load(args.model))
 
     print(f"Loaded checkpoint at {args.model}")
@@ -26,11 +36,3 @@ def get_flow_model_mft():
     model.eval()
 
     return model
-
-
-model = get_flow_model_mft()
-
-if __name__ == "__main__":
-    path_to_dataset = Path("data/360photo/original/concept/09")
-    # path_to_dataset = Path("RAFT/demo-frames")
-    export_flow_from_files(path_to_dataset, model)
