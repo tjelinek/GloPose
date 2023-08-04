e07d49b70e251a459383b78c3366c897826c6a3b
Tomas Jelinek <tomasjelinek96@gmail.com>
2023-08-04 14:34:38 +0200

    rendering the GoogleScannedObjects from more natural pose for datagen

--- dataset_generators/generate_google_scanned_objects_dataset.py
+++ dataset_generators/generate_google_scanned_objects_dataset.py
@@ -1,13 +1,14 @@
+import torch
 import warnings
 import types
 import os
 import shutil
 from pathlib import Path
 
-from dataset_generators.generator_utils import generate_rotating_textured_object
-from utils import load_config
+from dataset_generators.generator_utils import generate_rotating_textured_object, generate_rotations_x, \
+    generate_rotations_z
+from utils import load_config, deg_to_rad
 from main_settings import dataset_folder
-
 
 warnings.filterwarnings("ignore")
 
@@ -33,9 +34,12 @@ def dataset_from_google_research(config, dataset_path: Path):
             width = 1506
             height = 2000
 
+            initial_rotation_axis_angle = deg_to_rad(torch.Tensor([-90, 0, 0]).to('cuda'))
+
             generate_rotating_textured_object(config, mesh_path, texture_path, rendering_destination,
                                               segmentation_destination, optical_flow_destination, gt_tracking_log_file,
-                                              width, height)
+                                              width, height, initial_rotation_axis_angle=initial_rotation_axis_angle,
+                                              rotations=generate_rotations_z(5.0))
 
 
 if __name__ == '__main__':
