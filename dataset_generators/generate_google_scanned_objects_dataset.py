b6e3a751b86a7fb3ab96073220414095deb8ae92
Tomas Jelinek <tomasjelinek96@gmail.com>
2023-08-07 15:37:15 +0200

    allowed to generate multiple configurations for the Google Scanned objects dataset generator

--- dataset_generators/generate_google_scanned_objects_dataset.py
+++ dataset_generators/generate_google_scanned_objects_dataset.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import torch
 import warnings
 import types
@@ -13,7 +15,8 @@ from main_settings import dataset_folder
 warnings.filterwarnings("ignore")
 
 
-def dataset_from_google_research(config, dataset_path: Path):
+def dataset_from_google_research(config, dataset_path: Path, rotations, initial_rotation_axis_angle,
+                                 initial_translation):
     models_path = dataset_path / 'models'
     for file in os.listdir(models_path):
         d = models_path / file
@@ -34,13 +37,10 @@ def dataset_from_google_research(config, dataset_path: Path):
             width = 1000
             height = 1000
 
-            initial_rotation_axis_angle = deg_to_rad(torch.Tensor([-90, 0, 0]).to('cuda'))
-            initial_translation = torch.Tensor([0, 0, 0]).to('cuda')
-
             generate_rotating_textured_object(config, mesh_path, texture_path, rendering_destination,
                                               segmentation_destination, optical_flow_destination, gt_tracking_log_file,
                                               width, height, initial_rotation_axis_angle=initial_rotation_axis_angle,
-                                              rotations=generate_rotations_z(5.0),
+                                              rotations=rotations,
                                               initial_translation=initial_translation)
 
 
@@ -48,7 +48,23 @@ if __name__ == '__main__':
     config = load_config('configs/config_deep.yaml')
     config = types.SimpleNamespace(**config)
 
-    dataset_folder = dataset_folder / Path('GoogleScannedObjects')
+    GeneratorConfig = namedtuple('GeneratorConfig',
+                                 ['initial_rotation_axis_angle', 'initial_translation', 'rotations',
+                                  'name'])
 
-    dataset_from_google_research(config, dataset_folder)
+    configurations = {
+        'GoogleScannedObjects':
+            GeneratorConfig(initial_rotation_axis_angle=deg_to_rad(torch.Tensor([-90, 0, 0]).to('cuda')),
+                            initial_translation=torch.Tensor([0, 0, 0]).to('cuda'),
+                            rotations=generate_rotations_z(5.0), name='GoogleScannedObjects'),
+        'GoogleScannedObjects_default_pose':
+            GeneratorConfig(initial_rotation_axis_angle=deg_to_rad(torch.Tensor([0, 0, 0]).to('cuda')),
+                            initial_translation=torch.Tensor([0, 0, 0]).to('cuda'),
+                            rotations=generate_rotations_x(5.0), name='GoogleScannedObjects_default_pose'),
+    }
 
+    for gen_cfg_name, gen_cfg in configurations.items():
+        dataset_folder = dataset_folder / Path(gen_cfg_name)
+
+        dataset_from_google_research(config, dataset_folder, gen_cfg.rotations, gen_cfg.initial_rotation_axis_angle,
+                                     gen_cfg.initial_translation)
