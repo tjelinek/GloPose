ba4fe5f5bd4c9950fa319fb77fdf61b1a24a0007
Tomas Jelinek <tomasjelinek96@gmail.com>
2023-09-07 16:47:55 +0200

    enhanced the loss landscape visualization so that it shows all the axes

--- auxiliary_scripts/logging.py
+++ auxiliary_scripts/logging.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from typing import Dict
 
 import os
@@ -134,88 +136,114 @@ class WriteResults:
         gt_rotation_deg = rad_to_deg(tracking6d.gt_rotations[0, stepi]).cpu()
         gt_translation = tracking6d.gt_translations[0, 0, stepi].cpu()
 
-        translations_space = np.linspace(gt_translation[0] - 0.25, gt_translation[0] + 0.25, num=num_translations)
+        translations_space = np.linspace(gt_translation[0] - 0.3, gt_translation[0] + 0.3, num=num_translations)
         rotations_space = np.linspace(gt_rotation_deg[1] - 7, gt_rotation_deg[1] + 7, num=num_rotations)
 
-        joint_losses: np.ndarray = self.compute_loss_landscape(observed_flows, observed_flows_segmentations,
-                                                               observed_images, observed_segmentations, tracking6d,
-                                                               rotations_space, translations_space, stepi=stepi)
+        trans_axes = ['x', 'y', 'z']
+        rot_axes = ['x', 'y', 'z']
 
-        plt.figure(figsize=(10, 8))
-        plt.imshow(joint_losses.T,
-                   extent=[translations_space[0], translations_space[-1], rotations_space[-1], rotations_space[0]],
-                   aspect='auto', cmap='hot', interpolation='none')
-        plt.colorbar(label='Joint Loss')
-        plt.xlabel('Translation (x-axis)')
-        plt.ylabel('Rotation (degrees) (y-axis)')
+        for trans_axis_idx, rot_axis_idx in product(range(len(trans_axes)), range(len(rot_axes))):
 
-        # Set y-ticks for all integral numbers based on the range of rotations_space
-        plt.yticks(np.arange(int(rotations_space[0]), int(rotations_space[-1]) + 1, 1))
+            print(f"Visualizing loss landscape for translation axis {trans_axes[trans_axis_idx]} "
+                  f"and rotation axis {rot_axes[rot_axis_idx]}")
 
-        # Set x-ticks at every 0.1 step based on the range of translations_space
-        plt.xticks(np.arange(translations_space[0], translations_space[-1] + 0.1, 0.1))
+            joint_losses: np.ndarray = self.compute_loss_landscape(observed_flows, observed_flows_segmentations,
+                                                                   observed_images, observed_segmentations, tracking6d,
+                                                                   rotations_space, translations_space, trans_axis_idx,
+                                                                   rot_axis_idx, stepi)
 
-        # Add grid
-        plt.grid(True, which='both', linestyle='--', linewidth=0.25)
+            min_val = np.min(joint_losses)
+            max_val = np.max(joint_losses)
 
-        prev_iteration_x_translation = None
-        prev_iteration_y_rotation_deg = None
+            plt.figure(figsize=(10, 8))
+            plt.imshow(joint_losses.T,
+                       extent=[translations_space[0], translations_space[-1], rotations_space[-1], rotations_space[0]],
+                       aspect='auto', cmap='hot', interpolation='none', vmin=min_val, vmax=max_val)
+            cbar = plt.colorbar(label='Joint Loss')
+            ticks = list(cbar.get_ticks())
+            ticks.append(min_val)
+            ticks.append(max_val)
+            ticks = sorted(ticks)
+            cbar.set_ticks(ticks)
+            cbar.set_ticklabels([f'{tick:.2f}' for tick in ticks])
 
-        # TODO add more axes than x, y axis
-        for i in range(len(tracking6d.logged_sgd_translations)):
-            iteration_x_translation = tracking6d.logged_sgd_translations[i][0, 0, -1, 0].detach().cpu()
-            iteration_rotation_quaternion = tracking6d.logged_sgd_quaternions[i].detach().cpu()
-            iteration_rotation_rad = quaternion_to_angle_axis(iteration_rotation_quaternion,
-                                                              order=QuaternionCoeffOrder.WXYZ)
-            iteration_y_rotation_deg = rad_to_deg(iteration_rotation_rad)[0, 0, 1]
+            plt.xlabel(f'Translation ({trans_axes[trans_axis_idx]}-axis)')
+            plt.ylabel(f'Rotation (degrees) ({rot_axes[rot_axis_idx]}-axis)')
 
-            if i == 0:
-                plt.scatter(iteration_x_translation, iteration_y_rotation_deg, color='white', marker='x', label='Start')
-                plt.text(iteration_x_translation, iteration_y_rotation_deg, 'Start', verticalalignment='bottom',
-                         color='white')
-            elif i == len(tracking6d.logged_sgd_translations) - 1:
-                plt.scatter(iteration_x_translation, iteration_y_rotation_deg, color='yellow', marker='x', label='End')
-                plt.text(iteration_x_translation, iteration_y_rotation_deg, 'End', verticalalignment='bottom',
-                         color='yellow')
-                plt.plot([prev_iteration_x_translation, iteration_x_translation],
-                         [prev_iteration_y_rotation_deg, iteration_y_rotation_deg], color='yellow', linewidth=0.1,
-                         linestyle='dotted')
-            else:
-                plt.scatter(iteration_x_translation, iteration_y_rotation_deg, color='orange', marker='.', label=str(i))
-                plt.plot([prev_iteration_x_translation, iteration_x_translation],
-                         [prev_iteration_y_rotation_deg, iteration_y_rotation_deg], color='yellow', linewidth=0.1,
-                         linestyle='dotted')
+            # Set y-ticks for all integral numbers based on the range of rotations_space
+            plt.yticks(np.arange(int(rotations_space[0]), int(rotations_space[-1]) + 1, 1))
 
-            prev_iteration_x_translation = iteration_x_translation
-            prev_iteration_y_rotation_deg = iteration_y_rotation_deg
+            # Set x-ticks at every 0.1 step based on the range of translations_space
+            plt.xticks(np.arange(translations_space[0], translations_space[-1] + 0.1, 0.1))
 
-        plt.scatter(gt_translation[0], gt_rotation_deg[1], color='green', marker='x', label='GT')
-        plt.text(gt_translation[0], gt_rotation_deg[1], 'GT', verticalalignment='top', color='green')
+            # Add grid
+            plt.grid(True, which='both', linestyle='--', linewidth=0.25)
 
-        # 2) Show contours of the values
-        contours = plt.contour(translations_space, rotations_space, joint_losses.T, levels=20)
-        plt.clabel(contours, inline=True, fontsize=10)
+            prev_iteration_x_translation = None
+            prev_iteration_y_rotation_deg = None
 
-        # 3) Visualize the gradient
-        gradient = np.gradient(joint_losses.T)
-        # plt.quiver(translations_space, rotations_space, -gradient[1], gradient[0], color='white', width=0.003)
+            # TODO add more axes than x, y axis
+            for i in range(len(tracking6d.logged_sgd_translations)):
+                iteration_translation = tracking6d.logged_sgd_translations[i][0, 0, -1, trans_axis_idx].detach().cpu()
+                iteration_rotation_quaternion = tracking6d.logged_sgd_quaternions[i].detach().cpu()
+                iteration_rotation_rad = quaternion_to_angle_axis(iteration_rotation_quaternion,
+                                                                  order=QuaternionCoeffOrder.WXYZ)
+                iteration_rotation_deg = rad_to_deg(iteration_rotation_rad)[0, 0, rot_axis_idx]
 
-        plt.title('Joint Losses')
-        loss_landscape_folder = Path(tracking6d.write_folder / 'loss_landscapes')
-        loss_landscape_folder.mkdir(parents=True, exist_ok=True)
-        plt.savefig(loss_landscape_folder / f'joint_loss_landscape_{stepi}.eps', format='eps')
+                if i == 0:
+                    plt.scatter(iteration_translation, iteration_rotation_deg, color='white', marker='x', label='Start')
+                    plt.text(iteration_translation, iteration_rotation_deg, 'Start', verticalalignment='bottom',
+                             color='white')
+                elif i == len(tracking6d.logged_sgd_translations) - 1:
+                    plt.scatter(iteration_translation, iteration_rotation_deg, color='yellow', marker='x', label='End')
+                    plt.text(iteration_translation, iteration_rotation_deg, 'End', verticalalignment='bottom',
+                             color='yellow')
+                    plt.plot([prev_iteration_x_translation, iteration_translation],
+                             [prev_iteration_y_rotation_deg, iteration_rotation_deg], color='yellow', linewidth=0.1,
+                             linestyle='dotted')
+                else:
+                    plt.scatter(iteration_translation, iteration_rotation_deg, color='orange', marker='.', label=str(i))
+                    plt.plot([prev_iteration_x_translation, iteration_translation],
+                             [prev_iteration_y_rotation_deg, iteration_rotation_deg], color='yellow', linewidth=0.1,
+                             linestyle='dotted')
+
+                prev_iteration_x_translation = iteration_translation
+                prev_iteration_y_rotation_deg = iteration_rotation_deg
+
+            plt.scatter(gt_translation[trans_axis_idx], gt_rotation_deg[rot_axis_idx],
+                        color='green', marker='x', label='GT')
+            plt.text(gt_translation[trans_axis_idx], gt_rotation_deg[rot_axis_idx],
+                     'GT', verticalalignment='top', color='green')
+
+            # 2) Show contours of the values
+            contours = plt.contour(translations_space, rotations_space, joint_losses.T, levels=20)
+            plt.clabel(contours, inline=True, fontsize=10)
+
+            # 3) Visualize the gradient
+            gradient = np.gradient(joint_losses.T)
+            # plt.quiver(translations_space, rotations_space, -gradient[1], gradient[0], color='white', width=0.003)
+
+            plt.title('Joint Losses')
+            loss_landscape_folder = Path(tracking6d.write_folder / 'loss_landscapes')
+            loss_landscape_folder.mkdir(parents=True, exist_ok=True)
+            plt.savefig(loss_landscape_folder /
+                        f'joint_loss_landscape_{stepi}_trans-{trans_axes[trans_axis_idx]}'
+                        f'_rot-{rot_axes[rot_axis_idx]}.eps', format='eps')
 
     def compute_loss_landscape(self, observed_flows, observed_flows_segmentations, observed_images,
-                               observed_segmentations, tracking6d, rotation_x_space, translation_x_space, stepi):
+                               observed_segmentations, tracking6d, rotation_space, translation_space,
+                               trans_axis_idx, rot_axis_idx, stepi):
 
-        joint_losses = np.zeros((translation_x_space.shape[0], rotation_x_space.shape[0]))
+        joint_losses = np.zeros((translation_space.shape[0], rotation_space.shape[0]))
 
-        for i, translation_x in enumerate(translation_x_space):
-            for j, rotation_y_deg in enumerate(rotation_x_space):
-                translation_tensor = torch.Tensor([translation_x, 0, 0])
+        for i, translation in enumerate(translation_space):
+            for j, rotation_deg in enumerate(rotation_space):
+                translation_tensor = torch.Tensor([0, 0, 0])
+                translation_tensor[trans_axis_idx] = translation
                 sampled_translation = translation_tensor[None, None, None].cuda()
 
-                rotation_tensor_deg = torch.Tensor([0, rotation_y_deg, 0])
+                rotation_tensor_deg = torch.Tensor([0, 0, 0])
+                rotation_tensor_deg[rot_axis_idx] = rotation_deg
                 rotation_tensor_rad = deg_to_rad(rotation_tensor_deg)
                 rotation_tensor_quaternion = angle_axis_to_quaternion(rotation_tensor_rad,
                                                                       order=QuaternionCoeffOrder.WXYZ)
