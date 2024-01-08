14085eb052b910f53f9f1ec9e7c3d0c0b7102518
tjelinek <tomasjelinek96@gmail.com>
2024-01-08 17:47:32 +0100

    updated list of config experiments

--- scripts/all_experiments.sh
+++ scripts/all_experiments.sh
@@ -2,21 +2,19 @@
 
 # List of parameters (without the .yaml extension)
 params=(
-  "config_deep_no_flow_gt"
-  "config_deep_no_flow_stochastically_add_keyframes"
-  "config_deep_no_flow_tex_change_reg"
+  "config_deep_no_flow_all_frames_keyframes"
+  "config_deep_no_flow_gt_mesh"
   "config_deep_no_flow"
+  "config_deep_only_flow_gt_mesh"
   "config_deep_only_flow_gt_one_kf"
-  "config_deep_only_flow_gt"
-  "config_deep_only_iou_and_flow_gt_fewer_frames"
-  "config_deep_only_iou_and_flow_gt_max_keyframes"
+  "config_deep_only_flow_gt_two_kf"
+  "config_deep_only_flow"
+  "config_deep"
+  "config_deep_with_flow_all_frames_keyframes"
   "config_deep_with_flow_gt_no_rgb"
   "config_deep_with_flow_gt"
-  "config_deep_with_flow_max_keyframes"
-  "config_deep_with_flow_stochastically_add_keyframes"
-  "config_deep_with_flow_tex_change_reg"
+  "config_deep_with_flow_no_rgb"
   "config_deep_with_flow"
-  "config_deep"
   )
 
 # Loop through the parameters and run the sbatch command
