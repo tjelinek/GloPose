df3095c5d56fe7f0e5c31eaa0ab65cb19479427d
tjelinek <tomasjelinek96@gmail.com>
2024-03-04 10:46:02 +0100

    added experiments with essential_matrix_pose_estimation.py method

---
+++ configs/essential_matrix/config_deep_with_flow_gt_esmatrix_only_frontview_synthetic_8point.py
@@ -0,0 +1,17 @@
+from tracker_config import TrackerConfig
+
+
+def get_config() -> TrackerConfig:
+    cfg = TrackerConfig()
+
+    cfg.optimize_shape = False
+    cfg.all_frames_keyframes = False
+
+    cfg.matching_target_to_backview = False
+
+    cfg.run_main_optimization_loop = True
+    cfg.preinitialization_method = 'essential_matrix_decomposition'
+    cfg.gt_flow_source = 'GenerateSynthetic'
+    cfg.essential_matrix_algorithm = '8point'
+
+    return cfg
