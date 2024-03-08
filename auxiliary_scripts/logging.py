fixed coordinate calculation

auxiliary_scripts/logging.py
@@ -593,7 +593,8 @@ class WriteResults:
             random_sample = np.random.permutation(total_points)[:max_points]
             source_coords = source_coords[:, random_sample]
 
-        target_coords = source_coords_to_target_coords_np(source_coords, flow_frontview_np)
+        source_coords[0, :] = flow_np.shape[-2] - source_coords[0, :]
+        target_coords = source_coords_to_target_coords_np(source_coords, flow_np)
 
         norm = Normalize(vmin=0, vmax=source_coords.shape[1] - 1)
         cmap = plt.get_cmap(cmap)
@@ -604,8 +605,6 @@ class WriteResults:
 
             yA, xA = source_coords[:, i]
             yB, xB = target_coords[:, i]
-            yB = flow_frontview_np.shape[-2] - yB
-            yA = flow_frontview_np.shape[-2] - yA
 
             # Create a ConnectionPatch for each pair of sampled points
             axs1.plot(xA, yA, color=color, marker='X', markersize=0.5)

optim/essential_matrix_pose_estimation.py
@@ -12,15 +12,19 @@ def estimate_pose_using_dense_correspondences(dense_correspondences: torch.Tenso
                                               dense_correspondences_mask: torch.Tensor, W_world_to_cam,
                                               K1, K2, width: int, height: int, ransac_conf=0.99, method='magsac++'):
     # src and target in y, x order
-    src_pts = torch.nonzero(dense_correspondences_mask).to(torch.float32)
-    dst_pts = source_coords_to_target_coords(src_pts.permute(1, 0), dense_correspondences).permute(1, 0)
+    src_pts_yx = torch.nonzero(dense_correspondences_mask).to(torch.float32)
+    dst_pts_yx = source_coords_to_target_coords(src_pts_yx.permute(1, 0), dense_correspondences).permute(1, 0)
 
     # Convert to x, y order
-    src_pts[:, [0, 1]] = src_pts[:, [1, 0]]
-    dst_pts[:, [0, 1]] = dst_pts[:, [1, 0]]
+    src_pts_xy = src_pts_yx.clone()
+    dst_pts_xy = dst_pts_yx.clone()
+    src_pts_xy[:, [0, 1]] = src_pts_yx[:, [1, 0]]
+    dst_pts_xy[:, [0, 1]] = dst_pts_yx[:, [1, 0]]
+    # dst_pts_xy[:, 0] = height - dst_pts_xy[:, 0]
+    # TODO When converting to numpy, I may need to change the source coord to its mirror in the y axis
 
-    src_pts_np = src_pts.numpy(force=True)
-    dst_pts_np = dst_pts.numpy(force=True)
+    src_pts_np = src_pts_xy.numpy(force=True)
+    dst_pts_np = dst_pts_xy.numpy(force=True)
 
     if method == 'pygcransac':
         correspondences = np.ascontiguousarray(np.concatenate([src_pts_np, dst_pts_np], axis=1))
@@ -36,15 +40,15 @@ def estimate_pose_using_dense_correspondences(dense_correspondences: torch.Tenso
                                        prob=ransac_conf)
         mask = mask[:, 0]
 
-    inlier_src_pts = src_pts[torch.nonzero(torch.from_numpy(mask), as_tuple=True)]
-    outlier_src_pts = src_pts[torch.nonzero(~torch.from_numpy(mask), as_tuple=True)]
+    inlier_src_pts = src_pts_yx[torch.nonzero(torch.from_numpy(mask), as_tuple=True)]
+    outlier_src_pts = src_pts_yx[torch.nonzero(~torch.from_numpy(mask), as_tuple=True)]
 
     E_tensor = torch.from_numpy(E).cuda().to(torch.float32)
     K1_tensor = torch.from_numpy(K1).cuda()
     K2_tensor = torch.from_numpy(K2).cuda()
     mask_tensor = torch.from_numpy(mask).cuda()
     R, t, triangulated_points = motion_from_essential_choose_solution(E_tensor, K1_tensor, K2_tensor,
-                                                                      src_pts, dst_pts, mask_tensor)
+                                                                      src_pts_yx, dst_pts_yx, mask_tensor)
 
     T = Rt_to_matrix4x4(R.unsqueeze(0), t.unsqueeze(0))
 