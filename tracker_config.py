3e1e2a3cad5de6028401777278d7d324366d0790
tjelinek <tomasjelinek96@gmail.com>
2024-05-09 16:51:37 +0200

    using MFT iq

--- tracker_config.py
+++ tracker_config.py
@@ -100,7 +100,7 @@ class TrackerConfig:
 
     gt_flow_source: str = 'FlowNetwork'  # One of 'FlowNetwork', 'GenerateSynthetic'
     short_flow_model: str = 'GMA'  # 'RAFT' 'GMA'
-    long_flow_model: str = 'MFT'   # 'MFT' or None
+    long_flow_model: str = 'MFT_iq'   # 'MFT' or None
     MFT_backbone_cfg: str = 'MFT_cfg'
 
     matching_target_to_backview: bool = True
