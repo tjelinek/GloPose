52b251ef1edec4120a7c0e019a533e23757e7dc9
tjelinek <tomasjelinek96@gmail.com>
2024-05-16 16:37:59 +0200

    created base 8p algorithm tracking

--- configs/essential_matrix/config_deep_with_flow_gt_esmatrix_frontview_backview_flownet_8point.py
+++ configs/essential_matrix/config_deep_with_flow_gt_esmatrix_frontview_backview_flownet_8point.py
@@ -4,14 +4,19 @@ from tracker_config import TrackerConfig
 def get_config() -> TrackerConfig:
     cfg = TrackerConfig()
 
-    cfg.optimize_shape = False
-    cfg.all_frames_keyframes = False
+    cfg.ransac_essential_matrix_algorithm = None
 
-    cfg.matching_target_to_backview = True
+    cfg.ransac_use_gt_occlusions_and_segmentation = False
+    cfg.ransac_erode_segmentation_dilate_occlusion = False
 
-    cfg.run_main_optimization_loop = False
-    cfg.preinitialization_method = 'essential_matrix_decomposition'
-    cfg.gt_flow_source = 'FlowNetwork'
-    cfg.ransac_essential_matrix_algorithm = '8point'
+    cfg.ransac_feed_only_inlier_flow = False
+    cfg.ransac_replace_mft_flow_with_gt_flow = False
+    cfg.ransac_feed_gt_flow_add_gaussian_noise = False
+    cfg.ransac_feed_gt_flow_add_gaussian_noise_use_mft_errors = False
+    cfg.ransac_distant_pixels_sampling = False
+    cfg.ransac_confidences_from_occlusion = False
+    cfg.ransac_inlier_pose_method = '8point'
+
+    cfg.refine_pose_using_numerical_optimization = False
 
     return cfg
