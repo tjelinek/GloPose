edcde9699c22ee0689cd47df6f3e00c295a2818e
tjelinek <tomasjelinek96@gmail.com>
2024-05-20 23:22:48 +0200

    jobs with oracle flow with noisy sigma

---
+++ configs/mft_iq/mft_iq_synthetic_8p_pose_noise_sigma_005.py
@@ -0,0 +1,13 @@
+from configs.essential_matrix.config_deep_with_flow_gt_esmatrix_frontview_backview_flownet_8point import TrackerConfig8P
+
+
+def get_config() -> TrackerConfig8P:
+    cfg = TrackerConfig8P()
+
+    cfg.long_flow_model = 'MFT_Synth'
+    cfg.MFT_backbone_cfg = 'MFTIQ_SYNTHETIC_bs3_bce_200k_kubric_binary_cfg'
+    cfg.MFT_synth_add_noise = True
+    cfg.MFT_synth_noise_sigma = 0.05
+    cfg.MFT_synth_noise_mu = 0.
+
+    return cfg
