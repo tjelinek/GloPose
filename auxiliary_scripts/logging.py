0daeeb54a13b10e63a10f6b4cdaf981fa8a51987
tjelinek <tomasjelinek96@gmail.com>
2024-05-20 23:23:19 +0200

    dust3r mesh logging

--- auxiliary_scripts/logging.py
+++ auxiliary_scripts/logging.py
@@ -514,14 +514,18 @@ class WriteResults:
         triangulated_point_cloud_pred_flow_path = (self.point_clouds_path /
                                                    f'triangulated_point_cloud_gt_Rt_pred_flow_{frame_i}.ply')
         if self.tracking_config.write_to_rerun_rather_than_disk:
-            rr.log(RerunAnnotations.triangulated_points_gt_Rt_gt_flow,
-                   rr.Points3D(triangulated_point_cloud_gt_flow.numpy()))
-            rr.log(RerunAnnotations.triangulated_points_gt_Rt_mft_flow,
-                   rr.Points3D(triangulated_point_cloud_pred_flow.numpy()))
-            rr.log(RerunAnnotations.point_cloud_dust3r_im1,
-                   rr.Points3D(triangulated_point_cloud_dust3r_im1.numpy())),
-            rr.log(RerunAnnotations.point_cloud_dust3r_im2,
-                   rr.Points3D(triangulated_point_cloud_dust3r_im2.numpy()))
+            if triangulated_point_cloud_gt_flow is not None:
+                rr.log(RerunAnnotations.triangulated_points_gt_Rt_gt_flow,
+                       rr.Points3D(triangulated_point_cloud_gt_flow.numpy()))
+            if triangulated_point_cloud_pred_flow is not None:
+                rr.log(RerunAnnotations.triangulated_points_gt_Rt_mft_flow,
+                       rr.Points3D(triangulated_point_cloud_pred_flow.numpy()))
+            if triangulated_point_cloud_dust3r_im1 is not None:
+                rr.log(RerunAnnotations.point_cloud_dust3r_im1,
+                       rr.Points3D(triangulated_point_cloud_dust3r_im1.numpy()))
+            if triangulated_point_cloud_dust3r_im2 is not None:
+                rr.log(RerunAnnotations.point_cloud_dust3r_im2,
+                       rr.Points3D(triangulated_point_cloud_dust3r_im2.numpy()))
         else:
             save_ply(triangulated_point_cloud_gt_flow_path, triangulated_point_cloud_gt_flow)
             save_ply(triangulated_point_cloud_pred_flow_path, triangulated_point_cloud_pred_flow)
