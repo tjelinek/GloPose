23f3f9c929ac7681645e12ad4e452c7d1ff8cd8c
tjelinek <tomasjelinek96@gmail.com>
2024-05-21 11:46:36 +0200

    changed folder of configs

--- configs/epipolar/mft/mft_roma_8p_pose.py
+++ configs/epipolar/mft/mft_roma_8p_pose.py
@@ -1,4 +1,4 @@
-from configs.essential_matrix.config_deep_with_flow_gt_esmatrix_frontview_backview_flownet_8point import TrackerConfig8P
+from configs.epipolar.config_deep_with_flow_gt_esmatrix_frontview_backview_flownet_8point import TrackerConfig8P
 
 
 def get_config() -> TrackerConfig8P:
