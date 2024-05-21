309c850780d5a46934013e412f9962f292173519
tjelinek <tomasjelinek96@gmail.com>
2024-05-21 11:39:35 +0200

    added raft direct cfg

---
+++ configs/mft_iq/mft_iq_raft_direct_8p_pose.py
@@ -0,0 +1,10 @@
+from configs.essential_matrix.config_deep_with_flow_gt_esmatrix_frontview_backview_flownet_8point import TrackerConfig8P
+
+
+def get_config() -> TrackerConfig8P:
+    cfg = TrackerConfig8P()
+
+    cfg.long_flow_model = 'MFT_IQ'
+    cfg.MFT_backbone_cfg = 'MFTIQ_RAFT_bs3_bce_200k_kubric_binary_direct_cfg'
+
+    return cfg
