6b7e20f091f3a16b2a5afdc7dc2a7a95e5db35c5
tjelinek <tomasjelinek96@gmail.com>
2024-06-06 10:45:50 +0200

    extracted method

--- auxiliary_scripts/math_utils.py
+++ auxiliary_scripts/math_utils.py
@@ -1,15 +1,19 @@
 from kornia.geometry import inverse_transformation, Rt_to_matrix4x4, compose_transformations, matrix4x4_to_Rt
 
 
-def Rt_obj_from_epipolar_Rt_cam(R_cam, t_cam, T_world_to_cam):
-
-    T_cam = inverse_transformation(Rt_to_matrix4x4(R_cam, t_cam))
-
+def T_obj_from_epipolar_T_cam(T_cam, T_world_to_cam):
     T_cam_to_world = inverse_transformation(T_world_to_cam)
 
     # T_o1_to_o2 = T_w_to_c0 @ T_cam @ (T_w_to_c0)^-1
-    T_o2_to_o1 = compose_transformations(compose_transformations(T_world_to_cam, T_cam),
-                                         T_cam_to_world)
+    T_o2_to_o1 = compose_transformations(compose_transformations(T_world_to_cam, T_cam), T_cam_to_world)
+
+    return T_o2_to_o1
+
+
+def Rt_obj_from_epipolar_Rt_cam(R_cam, t_cam, T_world_to_cam):
+    T_cam = inverse_transformation(Rt_to_matrix4x4(R_cam, t_cam))
+
+    T_o2_to_o1 = T_obj_from_epipolar_T_cam(T_cam, T_world_to_cam)
 
     R, t = matrix4x4_to_Rt(T_o2_to_o1)
 
@@ -17,7 +21,6 @@ def Rt_obj_from_epipolar_Rt_cam(R_cam, t_cam, T_world_to_cam):
 
 
 def Rt_epipolar_cam_from_Rt_obj(R_obj, t_obj, T_world_to_cam):
-
     T_obj = Rt_to_matrix4x4(R_obj, t_obj)
     T_cam_to_world = inverse_transformation(T_world_to_cam)
 
