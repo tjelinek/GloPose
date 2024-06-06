415a5d8452e0db7fc202ddf1c0c9b6c24c8ba7ab
tjelinek <tomasjelinek96@gmail.com>
2024-06-06 10:38:17 +0200

    made more convenient

--- auxiliary_scripts/math_utils.py
+++ auxiliary_scripts/math_utils.py
@@ -3,11 +3,13 @@ from kornia.geometry import inverse_transformation, Rt_to_matrix4x4, compose_tra
 
 def Rt_obj_from_epipolar_Rt_cam(R_cam, t_cam, T_world_to_cam):
 
-    T_RANSAC = inverse_transformation(Rt_to_matrix4x4(R_cam, t_cam))
+    T_cam = inverse_transformation(Rt_to_matrix4x4(R_cam, t_cam))
 
-    # T_o2_to_o1 = T_w_to_c0 @ T_RANSAC @ (T_w_to_c0)^-1
-    T_o2_to_o1 = compose_transformations(compose_transformations(T_world_to_cam, T_RANSAC),
-                                         inverse_transformation(T_world_to_cam))
+    T_cam_to_world = inverse_transformation(T_world_to_cam)
+
+    # T_o2_to_o1 = T_w_to_c0 @ T_cam @ (T_w_to_c0)^-1
+    T_o2_to_o1 = compose_transformations(compose_transformations(T_world_to_cam, T_cam),
+                                         T_cam_to_world)
 
     R, t = matrix4x4_to_Rt(T_o2_to_o1)
 
@@ -15,9 +17,11 @@ def Rt_obj_from_epipolar_Rt_cam(R_cam, t_cam, T_world_to_cam):
 
 
 def Rt_epipolar_cam_from_Rt_obj(R_obj, t_obj, T_world_to_cam):
-    T_o2_to_o1 = Rt_to_matrix4x4(R_obj, t_obj)
-    T_w_to_c_inv = inverse_transformation(T_world_to_cam)
-    T_cam = compose_transformations(compose_transformations(T_w_to_c_inv, T_o2_to_o1), T_world_to_cam)
+
+    T_obj = Rt_to_matrix4x4(R_obj, t_obj)
+    T_cam_to_world = inverse_transformation(T_world_to_cam)
+
+    T_cam = compose_transformations(compose_transformations(T_cam_to_world, T_obj), T_world_to_cam)
 
     R_cam, t_cam = matrix4x4_to_Rt(T_cam)
 
