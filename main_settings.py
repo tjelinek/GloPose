ed33d38eead4d23f54cf5d2ef4e9dbfaef71da45
tjelinek <tomasjelinek96@gmail.com>
2024-06-16 21:42:08 +0200

    deleted unused

--- main_settings.py
+++ main_settings.py
@@ -13,22 +13,9 @@ if not os.path.exists(dataset_folder):
 	dataset_folder = str(Path('/mnt/personal/jelint19/data/').expanduser())
 	tmp_folder = str(Path('/mnt/personal/jelint19/results/FlowTracker/').expanduser())
 
-g_tbd_folder = dataset_folder+'TbD/'
-g_tbd3d_folder = dataset_folder+'TbD-3D/'
-g_falling_folder = dataset_folder+'falling_objects/'
-g_wildfmo_folder = dataset_folder+'wildfmo/'
-g_youtube_folder = dataset_folder+'youtube/'
-
-g_syn_folder = dataset_folder+'synthetic/'
-g_bg_folder = dataset_folder+'vot2018.zip'
-
 # TODO: download S2DNet weights:
 # wget https://www.dropbox.com/s/hnv51iwu4hn82rj/s2dnet_weights.pth -P /cluster/scratch/denysr/dataset/
 g_ext_folder = os.path.join(dataset_folder, 's2dnet_weights.pth')
 
-g_resolution_x = int(640/2)
-g_resolution_y = int(480/2)
 
-g_use_selfsupervised_timeconsistency = True
-g_timeconsistency_type = 'ncc'
 
