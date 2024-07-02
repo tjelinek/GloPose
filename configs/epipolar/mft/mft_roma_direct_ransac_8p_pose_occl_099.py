09a3bdf035faaf189b7ce6dadeb9b6cbf4b0f967
tjelinek <tomasjelinek96@gmail.com>
2024-07-02 10:37:46 +0200

    new jobs configs

---
+++ configs/epipolar/mft/mft_roma_direct_ransac_8p_pose_occl_099.py
@@ -0,0 +1,12 @@
+from configs.epipolar.config_deep_with_flow_gt_esmatrix_frontview_backview_flownet_8point import TrackerConfig8P
+
+
+def get_config() -> TrackerConfig8P:
+    cfg = TrackerConfig8P()
+
+    cfg.ransac_essential_matrix_algorithm = 'pygcransac'
+    cfg.long_flow_model = 'MFT'
+    cfg.MFT_backbone_cfg = 'RoMa_thresholds/MFT_RoMa_direct_cfg_occl_099'
+
+    cfg.occlusion_coef_threshold = 0.99
+    return cfg
