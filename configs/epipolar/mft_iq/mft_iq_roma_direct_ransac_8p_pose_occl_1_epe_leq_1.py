36e28ac6cad873037158923c08a77844b3f9e40e
tjelinek <tomasjelinek96@gmail.com>
2024-07-02 13:43:22 +0200

    new configs

---
+++ configs/epipolar/mft_iq/mft_iq_roma_direct_ransac_8p_pose_occl_1_epe_leq_1.py
@@ -0,0 +1,17 @@
+from configs.epipolar.config_deep_with_flow_gt_esmatrix_frontview_backview_flownet_8point import TrackerConfig8P
+
+
+def get_config() -> TrackerConfig8P:
+    cfg = TrackerConfig8P()
+
+    cfg.relative_camera_pose_algorithm = 'RANSAC_2D_to_2D_E_solver'
+    cfg.ransac_essential_matrix_algorithm = 'pygcransac'
+    cfg.long_flow_model = 'MFT_IQ'
+    cfg.MFT_backbone_cfg = 'MFTIQ_ROMA_bs3_bce_200k_kubric_binary_direct_cfg'
+
+    cfg.occlusion_coef_threshold = 1.0
+
+    cfg.ransac_feed_only_inlier_flow = True
+    cfg.ransac_feed_only_inlier_flow_epe_threshold = 1.0
+
+    return cfg
