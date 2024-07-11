416a26d7f86cc7ed1fc6399b0ec47452e857604c
tjelinek <tomasjelinek96@gmail.com>
2024-07-11 16:06:21 +0200

    using numpy rather

--- scripts/convert_old_pickles.py
+++ scripts/convert_old_pickles.py
@@ -1,14 +1,28 @@
 # Python 2.7 code
+import os
 import pickle
-import json
+import numpy as np
 
 
-def convert_pickle_to_json(pickle_file, json_file):
+def convert_pickle_to_npz(pickle_file, npz_file):
     with open(pickle_file, 'rb') as f:
         data = pickle.load(f)
 
-    with open(json_file, 'w') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    np.savez(npz_file, **data)
 
 
-convert_pickle_to_json('data.pkl', 'data.json')
+def process_folder(base_folder):
+    for subfolder in os.listdir(base_folder):
+        subfolder_path = os.path.join(base_folder, subfolder, 'meta')
+        if os.path.isdir(subfolder_path):
+            for file_name in os.listdir(subfolder_path):
+                if file_name.endswith('.pkl'):
+                    pickle_file = os.path.join(subfolder_path, file_name)
+                    npz_file = os.path.join(subfolder_path, file_name.replace('.pkl', '.npz'))
+                    convert_pickle_to_npz(pickle_file, npz_file)
+                    print(f"Converted {pickle_file} to {npz_file}")
+
+
+# Replace 'base_folder' with the path to your base folder
+base_folder = '/mnt/personal/jelint19/data/HO3D/train'
+process_folder(base_folder)
