0fbf20e7dfaef39968e4171205cd12fff0d86477
tjelinek <tomasjelinek96@gmail.com>
2024-07-11 17:47:08 +0200

    function moved to its own folder

--- segmentations.py
+++ segmentations.py
@@ -4,37 +4,13 @@ import cv2
 import torch
 import numpy as np
 
-from scipy.ndimage import uniform_filter
 from torchvision import transforms
-import torch.nn.functional as F
 
+from auxiliary_scripts.image_utils import pad_image, resize_and_filter_image
 from data_structures.keyframe_buffer import FrameObservation
 from models.encoder import Encoder
 from models.rendering import RenderingKaolin
 from tracker_config import TrackerConfig
-
-
-
-def pad_image(image):
-    W, H = image.shape[-2:]
-    max_size = max(H, W)
-    pad_h = max_size - H
-    pad_w = max_size - W
-    padding = [(pad_h + 1) // 2, pad_h // 2, pad_w // 2, (pad_w + 1) // 2]  # (top, bottom, left, right)
-
-    image = F.pad(image, padding, mode='constant', value=0)
-
-    return image
-
-
-def resize_and_filter_image(image, new_width, new_height):
-    image = cv2.resize(image, dsize=(new_width, new_height), interpolation=cv2.INTER_CUBIC)
-    image = uniform_filter(image, size=(3, 3, 1))
-
-    image_tensor = transforms.ToTensor()(image / 255.0)
-    image = image_tensor.unsqueeze(0).float()
-
-    return image
 
 
 class BaseTracker(ABC):
