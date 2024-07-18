48a846f7a2cee66d1ad7d30a1893da323d2b738a
tjelinek <tomasjelinek96@gmail.com>
2024-07-18 15:47:09 +0200

    changed interface of pnp pose estimator

--- pose/pnp_pose_estimation.py
+++ pose/pnp_pose_estimation.py
@@ -1,24 +1,15 @@
 import cv2
-import numpy as np
 import torch
 
-from tracker_config import TrackerConfig
-from utils import tensor_index_to_coordinates_xy
 
-
-def estimate_pose_using_PnP_solver(src_pts_yx: torch.Tensor, dst_pts_yx: torch.Tensor, K1: torch.Tensor,
-                                   K2: torch.Tensor, coords_c1_3d: torch.Tensor, width: int, height: int,
-                                   ransac_config: TrackerConfig, confidences=None):
-    # Convert to x, y order
-    src_pts_xy = tensor_index_to_coordinates_xy(src_pts_yx)
-    dst_pts_xy = tensor_index_to_coordinates_xy(dst_pts_yx)
+def estimate_pose_using_PnP_solver(src_pts_xy: torch.Tensor, dst_pts_xy: torch.Tensor, K1: torch.Tensor,
+                                   K2: torch.Tensor, coords_c1_3d_xy: torch.Tensor):
 
     src_pts_np = src_pts_xy.numpy(force=True)
     dst_pts_np = dst_pts_xy.numpy(force=True)
 
-    coords_c1_3d_fg = coords_c1_3d[0, 0, :, src_pts_yx[:, 1].long(), src_pts_yx[:, 0].long()]
-    coords_c1_3d_fg = coords_c1_3d_fg[[1, 0, 2]]
-    coords_c1_3d_numpy_fg = coords_c1_3d_fg.permute(1, 0).numpy(force=True)
+    coords_c1_3d_xy_fg = coords_c1_3d_xy[:, src_pts_xy[:, 0].long(), src_pts_xy[:, 1].long()]
+    coords_c1_3d_xy_numpy_fg = coords_c1_3d_xy_fg.permute(1, 0).numpy(force=True)
 
     K1_np = K1.numpy(force=True)
     K2_np = K2.numpy(force=True)
@@ -26,20 +17,15 @@ def estimate_pose_using_PnP_solver(src_pts_yx: torch.Tensor, dst_pts_yx: torch.T
     N_matches = src_pts_xy.shape[0]
     min_matches_for_ransac = 6
 
-    if N_matches > min_matches_for_ransac:
-        success, rvec, tvec, inliers = cv2.solvePnPRansac(coords_c1_3d_numpy_fg, dst_pts_np, K2_np, None)
+    if N_matches < min_matches_for_ransac:
+        raise ValueError("Not enough matches.")
 
-        rvec = torch.from_numpy(rvec).cuda().squeeze().to(torch.float32)
-        tvec = torch.from_numpy(tvec).cuda().to(torch.float32)
-        inliers = torch.from_numpy(inliers).cuda().to(torch.bool)[:, 0]
-    else:
-        tvec = torch.zeros(3).to(torch.float32).cuda().unsqueeze(-1)
-        rvec = torch.zeros(3).to(torch.float32).cuda()
+    success, camera_r, camera_t, inliers = cv2.solvePnPRansac(coords_c1_3d_xy_numpy_fg, dst_pts_np, K2_np, None)
 
-    inliers = torch.ones(src_pts_yx.shape[0], dtype=torch.bool)  # TODO remove me
-    triangulated_points = torch.zeros(inliers.shape[0], 3).to(torch.float32)  # TODO remove me
+    camera_r = torch.from_numpy(camera_r).cuda().squeeze().to(torch.float32)
+    camera_t = torch.from_numpy(camera_t).cuda().to(torch.float32)
+    inliers = torch.from_numpy(inliers).cuda().to(torch.bool)[:, 0]
 
-    rvec[1] -= np.pi
-    rvec *= -1.  # TODO properly align the poses
+    triangulated_points = coords_c1_3d_xy_numpy_fg[inliers]
 
-    return rvec, tvec, inliers, triangulated_points
+    return camera_r, camera_t, inliers, triangulated_points
