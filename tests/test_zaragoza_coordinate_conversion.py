86b623c4aa59a619069d0708b4b5cf7b8508bfbc
tjelinek <tomasjelinek96@gmail.com>
2024-07-18 10:22:41 +0200

    reformat

--- tests/test_zaragoza_coordinate_conversion.py
+++ tests/test_zaragoza_coordinate_conversion.py
@@ -11,7 +11,6 @@ from pose.essential_matrix_pose_estimation import estimate_pose_using_directly_z
 from tracker_config import TrackerConfig
 from utils import normalize_vertices, get_not_occluded_foreground_points, pinhole_intrinsics_from_tensor
 
-
 sequence_len = 2
 config = TrackerConfig()
 config.input_frames = sequence_len
@@ -31,24 +30,21 @@ gt_translation = torch.zeros(1, 1, sequence_len, 3).cuda()
 gt_encoder = Encoder(config, ivertices, iface_features, w, h, 3).cuda()
 gt_encoder.set_encoder_poses(gt_rotation, gt_translation)
 
-
 rendering = RenderingKaolin(config, faces, w, h).cuda()
 W_4x4 = rendering.camera_transformation_matrix_4x4()
 
 flow_observation = rendering.render_flow_for_frame(gt_encoder, 0, 1)
 
-
 src_pts_yx, observed_visible_fg_points_mask = (
-            get_not_occluded_foreground_points(flow_observation.rendered_flow_occlusion,
-                                               flow_observation.rendered_flow_segmentation,
-                                               config.occlusion_coef_threshold,
-                                               config.segmentation_mask_threshold))
+    get_not_occluded_foreground_points(flow_observation.rendered_flow_occlusion,
+                                       flow_observation.rendered_flow_segmentation,
+                                       config.occlusion_coef_threshold,
+                                       config.segmentation_mask_threshold))
 dst_pts_yx = source_coords_to_target_coords(src_pts_yx.permute(1, 0), flow_observation.theoretical_flow).permute(1, 0)
 camera_intrinsics = pinhole_intrinsics_from_tensor(rendering.camera_intrinsics, rendering.width, rendering.height)
 
-
 rot_cam, t_cam, inlier_mask = estimate_pose_using_directly_zaragoza(src_pts_yx, dst_pts_yx,
-                                                           camera_intrinsics.focal_x.cuda(),
-                                                           camera_intrinsics.focal_y.cuda(),
-                                                           camera_intrinsics.x0.cuda(),
-                                                           camera_intrinsics.y0.cuda())
+                                                                    camera_intrinsics.focal_x.cuda(),
+                                                                    camera_intrinsics.focal_y.cuda(),
+                                                                    camera_intrinsics.x0.cuda(),
+                                                                    camera_intrinsics.y0.cuda())
