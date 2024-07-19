fixed correspondences computation, which works on arbitrary shape of the input image - hurray

tests/test_zaragoza_coordinate_conversion.py
@@ -59,20 +59,11 @@ src_pts_yx, observed_visible_fg_points_mask = (
                                        config.segmentation_mask_threshold))
 
 flow = flow_unit_coords_to_image_coords(flow_observation.theoretical_flow)
-dst_pts_yx = source_coords_to_target_coords(src_pts_yx.permute(1, 0), flow).permute(1, 0)
+dst_pts_yx = source_coords_to_target_coords_world_frame(src_pts_yx.permute(1, 0), flow).permute(1, 0)
 
 src_pts_xy = src_pts_yx[..., [1, 0]]
 flow_xy = flow[:, :, [1, 0]].permute(0, 1, 2, 4, 3)
-dst_pts_xy = source_coords_to_target_coords(src_pts_xy.permute(1, 0), flow_xy).permute(1, 0)
-
-src_pts_xy2 = tensor_index_to_coordinates_xy(src_pts_yx)
-dst_pts_xy2 = tensor_index_to_coordinates_xy(dst_pts_yx)
-
-try:
-    assert torch.equal(src_pts_xy2, src_pts_xy) and torch.equal(dst_pts_xy2, dst_pts_xy)
-except:
-    print(f'dst_pts_yx2\n{dst_pts_xy}')
-    print(f'dst_pts_yx2\n{dst_pts_xy2}')
+dst_pts_xy = source_coords_to_target_coords_world_frame(src_pts_xy.permute(1, 0), flow_xy).permute(1, 0)
 
 K1 = rendering.camera_intrinsics
 