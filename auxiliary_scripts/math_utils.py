d7c3f0417cb8cea900185f00af2074031b043f3d
tjelinek <tomasjelinek96@gmail.com>
2024-08-14 14:11:09 +0200

    added pose conversion functions

--- auxiliary_scripts/math_utils.py
+++ auxiliary_scripts/math_utils.py
@@ -76,6 +76,22 @@ def camera_Se3_world_from_Se3_obj(Se3_obj: Se3, Se3_world_to_cam: Se3) -> Se3:
     return Se3_cam
 
 
+def get_camera_transform_for_frame(Se3_world_to_cam_1st_frame_: Se3, Se3_obj_1st_to_ref_: Se3) -> Se3:
+    Se3_cam_1st_to_ref_ = Se3_epipolar_cam_from_Se3_obj(Se3_obj_1st_to_ref_, Se3_world_to_cam_1st_frame_)
+    Se3_world_to_cam_ref_frame_ = Se3_world_to_cam_1st_frame_ * Se3_cam_1st_to_ref_
+
+    return Se3_world_to_cam_ref_frame_
+
+
+def get_rot_obj_from_reference(Se3_world_to_cam_1st_frame_: Se3, Se3_obj_1st_to_ref_: Se3, Se3_cam_ref_to_last_: Se3) \
+        -> Se3:
+
+    Se3_world_to_cam_ref_frame_ = get_camera_transform_for_frame(Se3_world_to_cam_1st_frame_, Se3_obj_1st_to_ref_)
+
+    Se3_obj_ref_to_last_ = Se3_obj_from_epipolar_Se3_cam(Se3_cam_ref_to_last_, Se3_world_to_cam_ref_frame_)
+
+    return Se3_obj_ref_to_last_
+
 def qmult(q1, q0):  # q0, then q1, you get q3
     w0, x0, y0, z0 = q0[0]
     w1, x1, y1, z1 = q1[0]
