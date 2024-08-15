c9f47f5ccf38387c894ec4502c85eaa6a2d6bc18
tjelinek <tomasjelinek96@gmail.com>
2024-08-15 09:35:47 +0200

    maybe more verastile test

--- tests/test_rot_coversion.py
+++ tests/test_rot_coversion.py
@@ -5,33 +5,54 @@ from kornia.geometry import axis_angle_to_rotation_matrix, rotation_matrix_to_ax
 
 from auxiliary_scripts.math_utils import Rt_obj_from_epipolar_Rt_cam, Rt_epipolar_cam_from_Rt_obj
 from dataset_generators import scenarios
+from dataset_generators.track_augmentation import modify_rotations
 from utils import homogenize_3x4_transformation_matrix
 
 for camera_up_idx in range(3):
+    scenario = scenarios.generate_rotations_xyz(5.0)
+
+    sequence_len = scenario.steps
+
+    scenario_t = scenarios.generate_xyz_translation(sequence_len)
+
+    gt_rotation = torch.from_numpy(scenario.rotation_axis_angles)[:sequence_len].cuda().to(torch.float32)  # * 0
+    gt_rotation[..., 0] *= -2.
+    gt_rotation[..., 1] *= 1.
+    gt_rotation[..., 2] *= -0.5
+
+    # gt_translation = torch.zeros(1, 1, sequence_len, 3).cuda()
+    gt_translation = torch.from_numpy(np.asarray(scenario_t.translations)).cuda()[:sequence_len].to(torch.float32) * 0
+    gt_translation[..., 0] *= 2.
+    gt_translation[..., 1] *= 1.
+    gt_translation[..., 2] *= 0.5
+
+    source_frame = 5
+    target_frame = 10
+
+    if True:
+        gt_rotation, gt_translation = modify_rotations(gt_rotation, gt_translation)
+        gt_rotation = gt_rotation[:sequence_len]
+        gt_translation = gt_translation[:sequence_len]
+
     camera_up = torch.tensor([0, 0, 0]).to(torch.float).unsqueeze(0)
     camera_up[:, camera_up_idx] = 1
     camera_translation = torch.tensor([4.0, -2.0, 5.0]).to(torch.float).unsqueeze(0)
     obj_center = torch.tensor([0, 0, 0]).to(torch.float).unsqueeze(0)
 
-    obj_rot_np = np.deg2rad(np.stack(scenarios.generate_rotations_xyz(5).rotations, axis=0))
-    obj_rot = torch.from_numpy(obj_rot_np).to(torch.float32)
-    # obj_rot[:, [0, 2]] *= -1.
-    t_obj = torch.zeros_like(obj_rot).squeeze().unsqueeze(-1)
-    t_obj[:, [2]] = 1.
-
     W_4x3 = kaolin.render.camera.generate_transformation_matrix(camera_position=camera_translation,
                                                                 camera_up_direction=camera_up,
-                                                                look_at=obj_center)
+                                                                look_at=obj_center).cuda()
 
-    T_world_to_cam = homogenize_3x4_transformation_matrix(W_4x3.permute(0, 2, 1)).repeat(obj_rot.shape[0], 1, 1)
+    T_world_to_cam = homogenize_3x4_transformation_matrix(W_4x3.permute(0, 2, 1)).repeat(gt_rotation.shape[0], 1, 1)
 
-    obj_R = axis_angle_to_rotation_matrix(obj_rot)
+    obj_R = axis_angle_to_rotation_matrix(gt_rotation)
 
-    R_cam, t_cam = Rt_epipolar_cam_from_Rt_obj(obj_R, t_obj, T_world_to_cam)
+    R_cam, t_cam = Rt_epipolar_cam_from_Rt_obj(obj_R, gt_translation.unsqueeze(-1), T_world_to_cam)
 
     r_cam = rotation_matrix_to_axis_angle(R_cam)
 
     R_obj_prime, t_obj_prime = Rt_obj_from_epipolar_Rt_cam(R_cam, t_cam, T_world_to_cam)
+    t_obj_prime = t_obj_prime.squeeze(-1)
 
     r_obj_prime = rotation_matrix_to_axis_angle(R_obj_prime)
 
@@ -42,11 +63,11 @@ for camera_up_idx in range(3):
         formatted_tensor = "\n".join(" ".join(f"{val:.3f}" for val in row) for row in tensor_numpy)
         print(f"{name}:\n{formatted_tensor}\n")
 
-    r_obj_print = torch.rad2deg(obj_rot)
+    r_obj_print = torch.rad2deg(gt_rotation)
     r_cam_print = torch.rad2deg(r_cam)
     r_obj_prime_print = torch.rad2deg(r_obj_prime)
 
-    R_obj = axis_angle_to_rotation_matrix(obj_rot)
+    R_obj = axis_angle_to_rotation_matrix(gt_rotation)
     so3_obj = So3.from_matrix(R_obj)
     so3_cam = So3.from_matrix(R_cam)
     so3_obj_prime = So3.from_matrix(R_obj_prime)
@@ -56,15 +77,15 @@ for camera_up_idx in range(3):
     rot_error_cam_obj_angle = torch.rad2deg(so3_rot_error_cam_obj.q.polar_angle)
 
     try:
-        assert torch.max(torch.abs(t_obj - t_obj_prime)) < 1e-3
+        assert torch.max(torch.abs(gt_translation - t_obj_prime)) < 1e-3
         assert torch.max(torch.abs(rot_error_angle)) < 1e-3
         # assert torch.max(torch.abs(rot_error_cam_obj_angle)) < 1e-3
     except:
         print("camera_ip", camera_up)
-        print_tensor("t_obj", t_obj[:5])
+        print_tensor("t_obj", gt_translation[:5])
         # print_tensor("t_cam", t_cam)
         print_tensor("t_obj_prime", t_obj_prime[:5])
         print_tensor("r_obj", r_obj_print[:5])
-        print_tensor("r_cam", r_cam_print[:5])
+        # print_tensor("r_cam", r_cam_print[:5])
         print_tensor("r_obj_prime", r_obj_prime_print[:5])
         breakpoint()
