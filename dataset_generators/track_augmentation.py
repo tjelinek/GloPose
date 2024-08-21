6a45add781534fc69c11a16d80c474347ff25127
tjelinek <tomasjelinek96@gmail.com>
2024-08-21 09:35:03 +0200

    better track augmentation

--- dataset_generators/track_augmentation.py
+++ dataset_generators/track_augmentation.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 
 
-def modify_rotations(gt_rotations, gt_translations, seed=42):
+def modify_rotations(gt_rotations, gt_translations, seed=42, stochastic_z=False):
     """
     Modify the given rotations in axis-angle representation.
 
@@ -21,10 +21,12 @@ def modify_rotations(gt_rotations, gt_translations, seed=42):
     N, _ = gt_rotations.shape
 
     # Extract y-dimension rotations
-    y_rotations = gt_rotations[:, 1] * 1.8
+    y_rotations = gt_rotations[:, 1]
 
     # Generate x-dimension rotations (twice as fast)
-    x_rotations = y_rotations * 0.05 * torch.sin(y_rotations * 5)
+    teeth = torch.deg2rad(torch.Tensor([5.])).repeat(N).cuda() * torch.Tensor([1., -1.]).repeat(N // 2).cuda()
+    teeth[0] = 0
+    x_rotations = y_rotations + teeth
 
     # Generate z-dimension rotations with random walk
     z_rotations = np.cumsum(np.deg2rad(np.random.uniform(-2, 8, N))).astype(np.float32)
