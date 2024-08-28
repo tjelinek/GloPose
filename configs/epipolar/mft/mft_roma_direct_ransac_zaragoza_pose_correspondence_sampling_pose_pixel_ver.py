a89eaea5c81c41e37433ff626b56ddc8aba8d41d
tjelinek <tomasjelinek96@gmail.com>
2024-08-28 17:20:31 +0200

    additional experiment

---
+++ configs/epipolar/mft/mft_roma_direct_ransac_zaragoza_pose_correspondence_sampling_pose_pixel_ver.py
@@ -0,0 +1,36 @@
+from tracker_config import TrackerConfig
+
+
+def get_config() -> TrackerConfig:
+    cfg = TrackerConfig()
+
+    cfg.run_main_optimization_loop = False
+    cfg.optimize_shape = False
+
+    cfg.ransac_inlier_filter = None
+
+    cfg.ransac_use_gt_occlusions_and_segmentation = False
+    cfg.ransac_dilate_occlusion = False
+    cfg.ransac_erode_segmentation = True
+
+    cfg.ransac_feed_only_inlier_flow = False
+    cfg.ransac_replace_mft_flow_with_gt_flow = False
+    cfg.ransac_feed_gt_flow_add_gaussian_noise = False
+    cfg.ransac_feed_gt_flow_add_gaussian_noise_use_mft_errors = False
+    cfg.ransac_distant_pixels_sampling = False
+    cfg.ransac_confidences_from_occlusion = False
+    cfg.ransac_inlier_pose_method = 'zaragoza'
+
+    cfg.ransac_refine_E_numerically = False
+
+    cfg.ransac_sample_points = True
+    cfg.ransac_sampled_points_number = 100
+
+    cfg.long_short_flow_chaining_pixel_level_verification = True
+    cfg.long_short_flow_chaining_pose_level_verification = True
+
+    cfg.ransac_inlier_filter = 'pygcransac'
+    cfg.long_flow_model = 'MFT'
+    cfg.MFT_backbone_cfg = 'MFT_RoMa_direct_cfg'
+
+    return cfg
