71463e5c34464f8d90cae88d58c34406114f5368
tjelinek <tomasjelinek96@gmail.com>
2024-09-24 20:54:38 +0200

    some renaming

--- pose/epipolar_pose_estimator.py
+++ pose/epipolar_pose_estimator.py
@@ -380,18 +380,18 @@ class EpipolarPoseEstimator:
     @staticmethod
     def recover_scale_with_rays(Se3_cam1_to_cam2_unscaled, Se3_world_to_cam, Se3_cam1_to_cam2_gt):
 
-        Se3_world_to_cam2 = Se3_cam1_to_cam2_unscaled * Se3_world_to_cam
+        Se3_world_to_cam2_unscaled = Se3_cam1_to_cam2_unscaled * Se3_world_to_cam
         ray_direction_cam1 = Se3_world_to_cam.translation
-        ray_direction_cam2 = Se3_world_to_cam2.translation
+        ray_direction_cam2 = Se3_world_to_cam2_unscaled.translation
 
         ray_direction_cam1_magnitude = torch.linalg.norm(ray_direction_cam1)
         ray_direction_cam2_magnitude = torch.linalg.norm(ray_direction_cam2)
 
         magnitude_ratio = ray_direction_cam1_magnitude / ray_direction_cam2_magnitude
         scaled_translation = Se3_cam1_to_cam2_unscaled.translation * magnitude_ratio
-        ray_direction_cam2_scaled = Se3(Se3_cam1_to_cam2_unscaled.quaternion, scaled_translation)
+        Se3_world_to_cam2_scaled = Se3(Se3_world_to_cam2_unscaled.quaternion, scaled_translation)
 
-        Se3_cam1_to_cam2_scaled = ray_direction_cam2_scaled * Se3_world_to_cam.inverse()
+        Se3_cam1_to_cam2_scaled = Se3_world_to_cam.inverse() * Se3_world_to_cam2_scaled
 
         return Se3_cam1_to_cam2_scaled
 
