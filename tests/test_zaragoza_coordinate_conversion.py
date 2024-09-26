aeb04f102bb8b2c1c7ea908fd566498940d8c0ec
tjelinek <tomasjelinek96@gmail.com>
2024-09-26 20:24:12 +0200

    added test for zaragoza pose conversion

--- tests/test_zaragoza_coordinate_conversion.py
+++ tests/test_zaragoza_coordinate_conversion.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
 import kaolin
-import numpy as np
 import torch
 from kornia.geometry import (Quaternion, rotation_matrix_to_axis_angle, Se3, quaternion_to_axis_angle)
 
-from auxiliary_scripts.math_utils import (Se3_last_cam_to_world_from_Se3_obj, Se3_obj_from_epipolar_Se3_cam)
+from auxiliary_scripts.math_utils import Se3_obj_from_epipolar_Se3_cam
 from dataset_generators import scenarios
 from dataset_generators.track_augmentation import modify_rotations
 from flow import flow_unit_coords_to_image_coords, source_to_target_coords_world_coord_system
 from models.encoder import Encoder
 from models.rendering import RenderingKaolin
+from pose.epipolar_pose_estimator import recover_scale
 from pose.essential_matrix_pose_estimation import estimate_pose_zaragoza
 from tracker_config import TrackerConfig
 from utils import normalize_vertices, get_not_occluded_foreground_points, erode_segment_mask2, \
@@ -19,9 +19,9 @@ from utils import normalize_vertices, get_not_occluded_foreground_points, erode_
 sequence_len = 30
 config = TrackerConfig()
 config.camera_up = (0, 1, 0)
-config.camera_position = (0, 0, 5)
+# config.camera_position = (0, 0, 5)
 # config.camera_position = (10, 0, 0)
-# config.camera_position = (-6, 5.18, 10)
+config.camera_position = (-6, 5.18, 10)
 config.input_frames = sequence_len
 
 path = Path('./prototypes/sphere.obj')
@@ -47,7 +47,7 @@ gt_translation[..., 2] *= 0.5
 
 first_frame = 0
 source_frame = 0
-target_frame = 2
+target_frame = 1
 
 if config.augment_gt_track or True:
     gt_rotation, gt_translation = modify_rotations(gt_rotation, gt_translation)
@@ -70,8 +70,8 @@ def predict_camera_pose_using_zaragoza(source_frame_, target_frame_, config_, re
     K1 = rendering_.camera_intrinsics
     rot_cam_, t_cam_ = estimate_pose_zaragoza(src_pts_xy, dst_pts_xy, K1[0, 0], K1[1, 1], K1[0, 2], K1[1, 2])
 
-    quat_cam_ = Quaternion.from_axis_angle(rot_cam_[None])
-    Se3_cam_ = Se3(quat_cam_, t_cam_[None, ..., 0])
+    quat_cam_ = Quaternion.from_axis_angle(rot_cam_)
+    Se3_cam_ = Se3(quat_cam_, t_cam_.squeeze(-1))
 
     return Se3_cam_
 
@@ -84,7 +84,7 @@ gt_encoder.set_encoder_poses(gt_rotation, gt_translation)
 
 rendering = RenderingKaolin(config, faces, w, h).cuda()
 
-Se3_world_to_cam_1st_frame = rendering.camera_transformation_matrix_Se3()
+Se3_world_to_cam = rendering.camera_transformation_matrix_Se3()
 
 quat_obj_gt = Quaternion.from_axis_angle(gt_rotation.to(torch.float32))
 Se3_obj_gt = Se3(quat_obj_gt, gt_translation)
@@ -98,7 +98,11 @@ Se3_cam_ref_to_last = predict_camera_pose_using_zaragoza(source_frame, target_fr
 Se3_cam_first_to_ref = predict_camera_pose_using_zaragoza(0, source_frame, config, rendering)
 Se3_cam_first_to_last = predict_camera_pose_using_zaragoza(0, target_frame, config, rendering)
 
-Se3_obj_ref_to_last_pred = Se3_obj_from_epipolar_Se3_cam(Se3_cam_ref_to_last, Se3_world_to_cam_1st_frame)
+Se3_cam_ref_to_last = recover_scale(Se3_cam_ref_to_last, Se3_world_to_cam)
+Se3_cam_first_to_ref = recover_scale(Se3_cam_first_to_ref, Se3_world_to_cam)
+Se3_cam_first_to_last = recover_scale(Se3_cam_first_to_last, Se3_world_to_cam)
+
+Se3_obj_ref_to_last_pred = Se3_obj_from_epipolar_Se3_cam(Se3_cam_ref_to_last, Se3_world_to_cam)
 
 rot_obj_ref_to_last_pred = rotation_matrix_to_axis_angle(Se3_obj_ref_to_last_pred.quaternion.matrix()).squeeze()
 t_obj_ref_to_last_pred = Se3_obj_ref_to_last_pred.translation.data.squeeze(-1)  # Shape (1, 3, 1) -> (1, 3)
@@ -123,9 +127,9 @@ print(f'Rot obj 1st to last pred: {torch.rad2deg(rot_obj_1st_to_last_pred).numpy
 print(f'Rot obj 1st to last   gt: {torch.rad2deg(rot_obj_1st_to_last_gt).numpy(force=True).round(3)}')
 print('----------------------------------------')
 
-print(f'T obj ref to last pred: {Se3_obj_ref_to_last_pred.translation.numpy(force=True).round(3)}')
+print(f'T obj ref to last pred: {Se3_obj_ref_to_last_pred.translation.numpy(force=True).round(3) / Se3_obj_ref_to_last_gt.translation.numpy(force=True)}')
 print(f'T obj ref to last   gt: {Se3_obj_ref_to_last_gt.translation.numpy(force=True).round(3)}')
-print(f'T obj 1st to last pred: {Se3_obj_1st_to_last_pred.translation.numpy(force=True).round(3)}')
+print(f'T obj 1st to last pred: {Se3_obj_1st_to_last_pred.translation.numpy(force=True).round(3) / Se3_obj_1st_to_last_gt.translation.numpy(force=True)}')
 print(f'T obj 1st to last   gt: {Se3_obj_1st_to_last_gt.translation.numpy(force=True).round(3)}')
 
 breakpoint()
