41ec7712729ac9224ae4d774feb760d54c1159b5
tjelinek <tomasjelinek96@gmail.com>
2024-10-07 16:32:04 +0200

    initial solution

---
+++ rotsym.py
@@ -0,0 +1,25 @@
+from sympy import symbols, Matrix, BlockMatrix, MatrixSymbol, ZeroMatrix
+from sympy.matrices import eye
+
+# Define symbols
+alpha = symbols('alpha', real=True)
+R_w2c = MatrixSymbol('R_w2c', 3, 3)  # This represents a matrix placeholder
+R_c = MatrixSymbol('R_c', 3, 3)
+t_w2c = MatrixSymbol('t_w2c', 3, 1)
+t_c = MatrixSymbol('t_c', 3, 1)
+
+# Define matrices using Matrix class
+T_w2c = BlockMatrix([[R_w2c, t_w2c], [ZeroMatrix(1, 3), Matrix([[1]])]])
+T_c = BlockMatrix([[R_c, t_c], [ZeroMatrix(1, 3), Matrix([[1]])]])
+
+# Alpha-scaled translation component of T_w2c
+T_w2c_alpha = Matrix([[R_w2c, alpha * t_w2c], [ZeroMatrix(1, 3), Matrix([[1]])]])
+
+# Construct the expression
+expr = T_w2c_alpha.inv() * T_c * T_w2c * (T_w2c_alpha.inv() * T_c * T_w2c_alpha)
+
+# Simplify the expression
+# simplified_expr = expr.simplify()
+
+print(expr)
+# print(simplified_expr)
