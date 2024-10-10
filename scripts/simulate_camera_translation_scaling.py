b02c34327f73492ea2aa64fee2990a58904ea0d2
tjelinek <tomasjelinek96@gmail.com>
2024-10-10 21:58:05 +0200

    logging pinholes

--- scripts/simulate_camera_translation_scaling.py
+++ scripts/simulate_camera_translation_scaling.py
@@ -9,7 +9,7 @@ from utils import homogenize_3x4_transformation_matrix
 
 camera_trans = torch.tensor([[3.14, -5.0, -2.81]]).cpu().to(torch.float32)
 camera_up = torch.tensor([[0, 0, 1]]).cpu().to(torch.float32)
-obj_center = torch.zeros((1, 3, )).cpu().to(torch.float32)
+obj_center = torch.zeros((1, 3,)).cpu().to(torch.float32)
 
 T_world_to_cam_4x3 = kaolin.render.camera.generate_transformation_matrix(camera_position=camera_trans,
                                                                          camera_up_direction=camera_up,
@@ -45,7 +45,6 @@ colors_scaled = [[0, 0, 255]] * 3
 strips_radii = [0.1] * 3
 
 for factor in torch.linspace(0, 2, 100).cpu():
-
     Se3_cam1_to_cam2_unscaled = Se3(rotation, translation * factor)
     Se3_obj1_to_cam2_unscaled = Se3_cam1_to_cam2_unscaled * Se3_obj_to_cam
     Se3_obj1_to_obj2_unscaled = Se3_obj_to_cam * Se3_obj1_to_cam2_unscaled
@@ -73,3 +72,32 @@ for factor in torch.linspace(0, 2, 100).cpu():
                            colors=colors_scaled,
                            radii=strips_radii))
 
+    # rr.log(
+    #     f'pose/camera1',
+    #     rr.Transform3D(translation=Se3_obj_to_cam.t.squeeze().numpy(force=True),
+    #                    rotation=rr.Quaternion(xyzw=Se3_obj_to_cam.quaternion.q.squeeze().numpy(force=True))),
+    #     from_parent=True,
+    # )
+    # rr.log(
+    #     f'pose/camera1',
+    #     rr.Pinhole(
+    #         resolution=[1, 1],
+    #         focal_length=[1., 1.],
+    #         camera_xyz=rr.ViewCoordinates.RUB,
+    #     ),
+    # )
+    #
+    # rr.log(f'pose/camera2',
+    #        rr.Transform3D(translation=Se3_obj1_to_cam2_unscaled.t.squeeze().numpy(force=True),
+    #                       rotation=rr.Quaternion(
+    #                           xyzw=Se3_obj1_to_cam2_unscaled.quaternion.q.squeeze().numpy(force=True))),
+    #        from_parent=True,
+    #        )
+    # rr.log(
+    #     f'pose/camera2',
+    #     rr.Pinhole(
+    #         resolution=[1, 1],
+    #         focal_length=[1., 1.],
+    #         camera_xyz=rr.ViewCoordinates.RUB,
+    #     ),
+    # )
