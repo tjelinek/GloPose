085e37a0d6e593f2a75c5bff09055cc7ca5cb720
tjelinek <tomasjelinek96@gmail.com>
2024-10-10 21:00:59 +0200

    added rotation conversions

--- scripts/simulate_camera_translation_scaling.py
+++ scripts/simulate_camera_translation_scaling.py
@@ -1,7 +1,7 @@
 import kaolin
 import rerun as rr
 import torch
-from kornia.geometry import Se3
+from kornia.geometry import Se3, Quaternion
 
 from utils import homogenize_3x4_transformation_matrix
 
@@ -13,7 +13,28 @@ T_world_to_cam_4x3 = kaolin.render.camera.generate_transformation_matrix(camera_
                                                                          camera_up_direction=camera_up,
                                                                          look_at=obj_center)
 T_world_to_cam_4x4 = homogenize_3x4_transformation_matrix(T_world_to_cam_4x3.permute(0, 2, 1))
-Se3_world_to_cam = Se3.from_matrix(T_world_to_cam_4x4)
+Se3_obj_to_cam = Se3.from_matrix(T_world_to_cam_4x4)
 
 #############################################################
 
+translation = torch.tensor([[1., 2., 3.]]).cuda()
+rotation = Quaternion.random(batch_size=1).cuda()
+
+position_cam1 = Se3_obj_to_cam.inverse().t.squeeze()
+
+Se3_cam1_to_cam2_scaled = Se3(rotation, translation)
+Se3_obj1_to_cam2_scaled = Se3_cam1_to_cam2_scaled * Se3_obj_to_cam
+Se3_obj1_to_obj2_scaled = Se3_obj_to_cam * Se3_obj1_to_cam2_scaled
+position_cam2_scaled = Se3_obj1_to_cam2_scaled.inverse().t.squeeze()
+position_obj2_scaled = Se3_obj1_to_obj2_scaled.inverse().t.squeeze()
+
+for factor in torch.linspace(0, 2, 100).cuda():
+
+    Se3_cam1_to_cam2_unscaled = Se3(rotation, translation * factor)
+    Se3_obj1_to_cam2_unscaled = Se3_cam1_to_cam2_unscaled * Se3_obj_to_cam
+    Se3_obj1_to_obj2_unscaled = Se3_obj_to_cam * Se3_obj1_to_cam2_unscaled
+
+    position_cam2_unscaled = Se3_obj1_to_cam2_unscaled.inverse().t.squeeze()
+    position_obj2_unscaled = Se3_obj1_to_obj2_unscaled.inverse().t.squeeze()
+
+    breakpoint()
