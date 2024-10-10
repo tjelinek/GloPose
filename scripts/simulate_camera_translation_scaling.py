88a4d904287215cf1a8761a5b7a8eedaa3c0d913
tjelinek <tomasjelinek96@gmail.com>
2024-10-10 21:40:54 +0200

    logging line strips

--- scripts/simulate_camera_translation_scaling.py
+++ scripts/simulate_camera_translation_scaling.py
@@ -7,9 +7,9 @@ from kornia.geometry import Se3, Quaternion
 
 from utils import homogenize_3x4_transformation_matrix
 
-camera_trans = torch.tensor([[3.14, -5.0, -2.81]]).cuda().to(torch.float32)
-camera_up = torch.tensor([[0, 0, 1]]).cuda().to(torch.float32)
-obj_center = torch.zeros((1, 3, )).cuda().to(torch.float32)
+camera_trans = torch.tensor([[3.14, -5.0, -2.81]]).cpu().to(torch.float32)
+camera_up = torch.tensor([[0, 0, 1]]).cpu().to(torch.float32)
+obj_center = torch.zeros((1, 3, )).cpu().to(torch.float32)
 
 T_world_to_cam_4x3 = kaolin.render.camera.generate_transformation_matrix(camera_position=camera_trans,
                                                                          camera_up_direction=camera_up,
@@ -29,8 +29,8 @@ rr.send_blueprint(blueprint)
 
 ###############################################################
 
-translation = torch.tensor([[1., 2., 3.]]).cuda()
-rotation = Quaternion.random(batch_size=1).cuda()
+translation = torch.tensor([[1., 2., 3.]]).cpu()
+rotation = Quaternion.random(batch_size=1).cpu()
 
 position_cam1 = Se3_obj_to_cam.inverse().t.squeeze()
 
@@ -40,11 +40,11 @@ Se3_obj1_to_obj2_scaled = Se3_obj_to_cam * Se3_obj1_to_cam2_scaled
 position_cam2_scaled = Se3_obj1_to_cam2_scaled.inverse().t.squeeze()
 position_obj2_scaled = Se3_obj1_to_obj2_scaled.inverse().t.squeeze()
 
-colors_unscaled = [[0., 255., 0.]] * 3
-colors_scaled = [[0., 0., 255.]] * 3
+colors_unscaled = [[0, 255, 0]] * 3
+colors_scaled = [[0, 0, 255]] * 3
 strips_radii = [0.1] * 3
 
-for factor in torch.linspace(0, 2, 100).cuda():
+for factor in torch.linspace(0, 2, 100).cpu():
 
     Se3_cam1_to_cam2_unscaled = Se3(rotation, translation * factor)
     Se3_obj1_to_cam2_unscaled = Se3_cam1_to_cam2_unscaled * Se3_obj_to_cam
@@ -55,16 +55,21 @@ for factor in torch.linspace(0, 2, 100).cuda():
 
     rr.set_time_sequence('scale_factor', int(factor * 100))
 
-    breakpoint()
-    line_strip_unscaled = np.stack([obj_center[0], position_cam1, position_cam2_unscaled, position_obj2_unscaled])
-    line_strip_scaled = np.stack([obj_center[0], position_cam1, position_cam2_scaled, position_obj2_scaled])
+    line_strip_unscaled = np.stack([obj_center[0], position_cam1.numpy(force=True),
+                                    position_cam2_unscaled.numpy(force=True),
+                                    position_obj2_unscaled.numpy(force=True)])
+
+    line_strip_scaled = np.stack([obj_center[0], position_cam1.numpy(force=True),
+                                  position_cam2_scaled.numpy(force=True),
+                                  position_obj2_scaled.numpy(force=True)])
 
     rr.log('pose/unscaled_rotation',
-           rr.LineStrips3D(strips=line_strip_unscaled,  # gt_t_cam
+           rr.LineStrips3D(strips=line_strip_unscaled,
                            colors=colors_unscaled,
                            radii=strips_radii))
 
-    rr.log('pose/unscaled_rotation',
-           rr.LineStrips3D(strips=line_strip_scaled,  # pred_t_cam
+    rr.log('pose/scaled_rotation',
+           rr.LineStrips3D(strips=line_strip_scaled,
                            colors=colors_scaled,
                            radii=strips_radii))
+
