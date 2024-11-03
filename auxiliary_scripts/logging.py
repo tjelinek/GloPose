7711f5f5e7f0b9dfa45021c6a8f72d74d5121520
tjelinek <tomasjelinek96@gmail.com>
2024-11-03 18:22:28 +0100

    rather carefully copying

--- auxiliary_scripts/logging.py
+++ auxiliary_scripts/logging.py
@@ -267,7 +267,7 @@ class WriteResults:
         self.colmap_db.create_tables()
 
         colmap_db_camera_params = (np.array([self.pinhole_params.fx.item(), self.pinhole_params.fy.item(),
-                                            self.pinhole_params.cx.item(), self.pinhole_params.cy.item()]).
+                                             self.pinhole_params.cx.item(), self.pinhole_params.cy.item()]).
                                    astype(np.float64))
         self.colmap_db.add_camera(1, self.image_width, self.image_height, colmap_db_camera_params, camera_id=1)
 
@@ -527,12 +527,12 @@ class WriteResults:
 
         for template_annotation in self.template_fields:
             rr.log(template_annotation,
-                       rr.SeriesPoint(
-                           color=[255, 0, 0],
-                           name="new template",
-                           marker="circle",
-                           marker_size=4,
-                       ),
+                   rr.SeriesPoint(
+                       color=[255, 0, 0],
+                       name="new template",
+                       marker="circle",
+                       marker_size=4,
+                   ),
                    static=True)
 
         rr.send_blueprint(blueprint)
@@ -856,7 +856,7 @@ class WriteResults:
 
         rr.set_time_sequence(RerunAnnotations.space_visualization, frame_i)
 
-        all_frames_from_0 = range(0, frame_i+1)
+        all_frames_from_0 = range(0, frame_i + 1)
         n_poses = len(all_frames_from_0)
 
         T_world_to_cam_se3 = Se3.from_matrix(self.pinhole_params.extrinsics)
@@ -1013,12 +1013,22 @@ class WriteResults:
         imageio.v3.imwrite(node_save_path, (img * 255).to(torch.uint8))
 
         seg_target_nonzero = img_seg[..., 0].nonzero()
-        seg_target_nonzero_unit = seg_target_nonzero#pixel_coords_to_unit_coords(self.image_width, self.image_height, seg_target_nonzero)
+        seg_target_nonzero_unit = seg_target_nonzero  #pixel_coords_to_unit_coords(self.image_width, self.image_height, seg_target_nonzero)
         seg_target_nonzero_xy_np = seg_target_nonzero_unit[..., [1, 0]].numpy(force=True).astype(np.float32)
         assert seg_target_nonzero_xy_np.dtype == np.float32
 
-        self.colmap_db.add_image(name=f'./{str(node_save_path.name)}', camera_id=1, image_id=frame_idx+1)
-        self.colmap_db.add_keypoints(frame_idx+1, seg_target_nonzero_xy_np)
+        general_fame_data = self.data_graph.get_frame_data(frame_idx)
+        gt_t_obj = general_fame_data.gt_translation[None]
+        gt_r_obj = general_fame_data.gt_rot_axis_angle[None]
+        gt_Se3_obj = Se3(Quaternion.from_axis_angle(gt_r_obj), gt_t_obj)
+        Se3_world_to_cam = Se3.from_matrix(self.pinhole_params.extrinsics)
+        gt_Se3_cam = Se3_epipolar_cam_from_Se3_obj(gt_Se3_obj, Se3_world_to_cam)
+        gt_q_cam_np = gt_Se3_cam.quaternion.q.squeeze().numpy(force=True).copy()
+        gt_t_cam_np = gt_Se3_cam.t.squeeze().numpy(force=True).copy()
+
+        self.colmap_db.add_image(name=f'./{str(node_save_path.name)}', camera_id=1, image_id=frame_idx + 1,
+                                 prior_q=gt_q_cam_np, prior_t=gt_t_cam_np)
+        self.colmap_db.add_keypoints(frame_idx + 1, seg_target_nonzero_xy_np.copy())
 
         icosphere_nodes_idx = {node.keyframe_idx_observed for node in self.pose_icosphere.reference_poses}
         incoming_edges = self.data_graph.G.in_edges(frame_idx)
@@ -1045,8 +1055,8 @@ class WriteResults:
             src_pts_indices_filtered = src_pts_indices[dst_pts_mask]
 
             matches = torch.stack([src_pts_indices_filtered, dst_pts_indices], dim=1)
-            self.colmap_db.add_matches(edge_source+1, edge_target+1, matches.numpy(force=True))
-            self.colmap_db.add_two_view_geometry(edge_source+1, edge_target+1, matches.numpy(force=True))
+            self.colmap_db.add_matches(edge_source + 1, edge_target + 1, matches.numpy(force=True).copy())
+            self.colmap_db.add_two_view_geometry(edge_source + 1, edge_target + 1, matches.numpy(force=True).copy())
 
         self.colmap_db.commit()
 
