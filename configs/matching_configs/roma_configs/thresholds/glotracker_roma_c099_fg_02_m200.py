e5de0b46eaf998fe7dac0d6a7b6f35edbd424bc9
tjelinek <tomasjelinek96@gmail.com>
2025-01-13 16:32:12 +0100

    rem unused param

--- configs/matching_configs/roma_configs/thresholds/glotracker_roma_c099_fg_02_m200.py
+++ configs/matching_configs/roma_configs/thresholds/glotracker_roma_c099_fg_02_m200.py
@@ -4,8 +4,6 @@ from tracker_config import TrackerConfig
 def get_config() -> TrackerConfig:
     cfg = TrackerConfig()
 
-    cfg.optimize_shape = False
-
     cfg.frame_filter = 'RoMa'
 
     cfg.min_roma_certainty_threshold = 0.99
