a7f7c522329d61ff9fd48ad6fc45252fdc381895
tjelinek <tomasjelinek96@gmail.com>
2025-01-17 15:22:02 +0100

    base kaolin renderer config

---
+++ configs/base_renderer_config.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class BaseKaolinRendererConfig:
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        self.config_name: str = self.__class__.__name__
