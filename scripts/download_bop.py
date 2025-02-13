b4b9cd4e2c275c1cdaa2dc89b635669f0f124025
tjelinek <tomasjelinek96@gmail.com>
2025-02-13 14:03:26 +0100

    fixed bop downloader to new format

--- scripts/download_bop.py
+++ scripts/download_bop.py
@@ -1,12 +1,13 @@
 from pathlib import Path
-
 from huggingface_hub import snapshot_download
 
 dataset_name = "hot3d"
-local_dir = Path("/mnt/personal/jelint19/data/bop")
+local_dir = Path(f"/mnt/personal/jelint19/data/bop/{dataset_name}")
 local_dir.mkdir(parents=True, exist_ok=True)
 
-snapshot_download(repo_id="bop-benchmark/datasets",
-                  allow_patterns=f"{dataset_name}/*zip",
-                  repo_type="dataset",
-                  local_dir=local_dir)
+snapshot_download(
+    repo_id=f"bop-benchmark/{dataset_name}",
+    allow_patterns=["test_aria/*.tar"],
+    repo_type="dataset",
+    local_dir=local_dir
+)
