d6930466e60f3234f8a1b15b5cc8eefa01c57d47
tjelinek <tomasjelinek96@gmail.com>
2025-02-17 14:05:33 +0100

    running onboarding basics

--- run_HOPE.py
+++ run_HOPE.py
@@ -14,17 +14,17 @@ from tracker6d import run_tracking_on_sequence
 
 
 def main():
-    dataset = 'HANDAL'
+    dataset = 'HOPE'
     args = parse_args()
     if args.sequences is not None and len(args.sequences) > 0:
         sequences = args.sequences
     else:
         sequences = [
-            '000001',
-            '000002',
-            '000003',
-            '000004',
-            '000005',
+            'obj_000001',  'obj_000006',  'obj_000011',  'obj_000016',  'obj_000021',  'obj_000026',
+            'obj_000002',  'obj_000007',  'obj_000012',  'obj_000017',  'obj_000022',  'obj_000027',
+            'obj_000003',  'obj_000008',  'obj_000013',  'obj_000018',  'obj_000023',  'obj_000028',
+            'obj_000004',  'obj_000009',  'obj_000014',  'obj_000019',  'obj_000024',
+            'obj_000005',  'obj_000010',  'obj_000015',  'obj_000020',  'obj_000025',
         ]
 
     for sequence in sequences:
@@ -38,12 +38,12 @@ def main():
         config.experiment_name = experiment_name
         config.sequence = sequence
         config.dataset = dataset
-        config.image_downsample = 0.5
+        config.image_downsample = 1.0
 
         # config.camera_position = (-5.0, -5.0, -5.0)
         # config.camera_up = (0, 0, 1)
 
-        skip_indices = 4
+        skip_indices = 1
 
         if args.output_folder is not None:
             write_folder = Path(args.output_folder) / dataset / sequence
@@ -52,7 +52,9 @@ def main():
 
         t0 = time.time()
 
-        sequence_folder = config.default_data_folder / 'bop' / 'handal' / 'val' / sequence
+        onboarding_type = config.bop_config.onboarding_type
+
+        sequence_folder = config.default_data_folder / 'bop' / 'hope' / f'onboarding_{onboarding_type}' / sequence
         image_folder = sequence_folder / 'rgb'
         segmentation_folder = sequence_folder / 'mask_visib'
 
@@ -77,7 +79,6 @@ def main():
 
                 cam_intrinsics[int(frame)] = K
 
-        # config.camera_intrinsics = cam_intrinsics[0]
         pinhole_params = get_pinhole_params(sequence_folder / 'scene_camera.json')
         config.camera_intrinsics = pinhole_params[0].intrinsics.squeeze().numpy(force=True)
         config.camera_extrinsics = pinhole_params[0].extrinsics.squeeze().numpy(force=True)
@@ -92,11 +93,6 @@ def main():
         gt_images = [gt_images[i] for i in valid_indices]
         gt_segs = [gt_segs[i] for i in valid_indices]
 
-        config.input_frames = len(gt_images)
-
-        config.frame_provider = 'precomputed'
-        config.segmentation_provider = 'SAM2'
-
         gt_rotations_obj_to_cam = torch.from_numpy(np.array(filtered_gt_rotations_obj_i_to_obj_i))
         gt_translations_obj_to_cam = torch.from_numpy(np.array(filtered_gt_translations_obj_1_to_obj_i))
 
@@ -107,6 +103,10 @@ def main():
         gt_Se3_cam_to_obj = gt_Se3_obj_to_cam.inverse()
 
         first_image, first_segmentation = get_initial_image_and_segment(gt_images, gt_segs, segmentation_channel=0)
+
+        config.input_frames = len(gt_images)
+        config.frame_provider = 'precomputed'
+        config.segmentation_provider = 'SAM2'
 
         print('Data loading took {:.2f} seconds'.format((time.time() - t0) / 1))
 
