051d3002b6dc5723624d1118be8c1a06d6d6e4a4
tjelinek <tomasjelinek96@gmail.com>
2025-02-24 20:04:30 +0100

    deleted unused config

--- configs/glotracker/glotracker_sift.py
+++ configs/glotracker/glotracker_sift.py
@@ -4,8 +4,6 @@ from tracker_config import TrackerConfig
 def get_config() -> TrackerConfig:
     cfg = TrackerConfig()
 
-    cfg.optimize_shape = False
-
     cfg.frame_filter = 'SIFT'
 
     return cfg
