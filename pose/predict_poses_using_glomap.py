287c53ef166d553012595ef124c7731b90e43f84
tjelinek <tomasjelinek96@gmail.com>
2025-02-25 13:54:11 +0100

    file for predicting poses using glomap

---
+++ pose/predict_poses_using_glomap.py
@@ -0,0 +1,8 @@
+import torch
+
+from data_structures.view_graph import ViewGraph
+
+
+def predict_poses(image: torch.Tensor, segmentation: torch.Tensor, view_graph: ViewGraph) -> torch.Tensor:
+
+    pass
