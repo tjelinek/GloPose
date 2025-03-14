e8029079250bef5f71aff953ce65140b3f9361c1
tjelinek <tomasjelinek96@gmail.com>
2025-03-14 15:49:19 +0100

    run_HANDAL.py remade into new format

--- run_HANDAL.py
+++ run_HANDAL.py
@@ -1,16 +1,11 @@
 import time
 from pathlib import Path
 
-import numpy as np
-import torch
-from kornia.geometry import Se3, Quaternion
-
 from utils.data_utils import get_initial_image_and_segment
-from utils.dataset_utils.bop_challenge import get_pinhole_params, read_obj_to_cam_transformations_from_gt, \
-    load_gt_images_and_segmentations
+from utils.bop_challenge import get_pinhole_params, load_gt_images_and_segmentations, extract_gt_Se3_cam2obj
 from utils.general import load_config
 from utils.runtime_utils import parse_args
-from tracker6d import run_tracking_on_sequence
+from tracker6d import Tracker6D
 
 
 def main():
@@ -40,7 +35,7 @@ def main():
         config.dataset = dataset
         config.image_downsample = 0.5
 
-        skip_indices = 4
+        skip_indices = 1
 
         if args.output_folder is not None:
             write_folder = Path(args.output_folder) / dataset / sequence
@@ -57,45 +52,31 @@ def main():
 
         pose_json_path = sequence_folder / 'scene_gt.json'
 
-        gt_rotations_objs_to_cam, gt_translations_objs_to_cam = read_obj_to_cam_transformations_from_gt(pose_json_path)
-        obj_ids = sorted(gt_rotations_objs_to_cam.keys())
-        gt_rotations_obj_to_cam_dict = gt_rotations_objs_to_cam[obj_ids[0]]
-        gt_translations_obj_to_cam_dict = gt_translations_objs_to_cam[obj_ids[0]]
+        dict_gt_Se3_cam2obj = extract_gt_Se3_cam2obj(pose_json_path, None, config.device)
+        gt_Se3_obj2cam_frame0 = dict_gt_Se3_cam2obj[min(dict_gt_Se3_cam2obj.keys())]
 
-        pinhole_params = get_pinhole_params(sequence_folder / 'scene_camera.json')
-        valid_indices = list(sorted(gt_segs.keys() & gt_images.keys() & gt_translations_obj_to_cam_dict.keys() &
-                                    gt_rotations_obj_to_cam_dict.keys()))
-        valid_indices = valid_indices[::skip_indices]
-
-        # Filter the lists to include only valid elements
-        filtered_gt_rotations_obj_i_to_obj_i = [gt_rotations_obj_to_cam_dict[i] for i in valid_indices]
-        filtered_gt_translations_obj_1_to_obj_i = [gt_translations_obj_to_cam_dict[i] for i in valid_indices]
+        valid_indices = sorted(list(dict_gt_Se3_cam2obj.keys()))[::skip_indices]
         gt_images = [gt_images[i] for i in valid_indices]
         gt_segs = [gt_segs[i] for i in valid_indices]
-
-        config.input_frames = len(gt_images)
-
-        config.camera_intrinsics = pinhole_params[0].intrinsics.squeeze().numpy(force=True)
-        config.camera_extrinsics = pinhole_params[0].extrinsics.squeeze().numpy(force=True)
-        config.frame_provider = 'precomputed'
-        config.segmentation_provider = 'SAM2'
-
-        gt_rotations_obj_to_cam = torch.from_numpy(np.array(filtered_gt_rotations_obj_i_to_obj_i))
-        gt_translations_obj_to_cam = torch.from_numpy(np.array(filtered_gt_translations_obj_1_to_obj_i))
-
-        gt_rotations_obj_to_cam = gt_rotations_obj_to_cam.to(torch.float32).to(config.device)
-        gt_translations_obj_to_cam = gt_translations_obj_to_cam.to(torch.float32).to(config.device)
-
-        gt_Se3_obj_to_cam = Se3(Quaternion.from_axis_angle(gt_rotations_obj_to_cam), gt_translations_obj_to_cam)
-        gt_Se3_cam_to_obj = gt_Se3_obj_to_cam.inverse()
+        dict_gt_Se3_cam2obj = {i: dict_gt_Se3_cam2obj[frame] for i, frame in enumerate(valid_indices)}
 
         first_image, first_segmentation = get_initial_image_and_segment(gt_images, gt_segs, segmentation_channel=0)
 
+        pinhole_params = get_pinhole_params(sequence_folder / 'scene_camera.json')
+
+        config.camera_intrinsics = pinhole_params[min(valid_indices)].intrinsics.squeeze().numpy(force=True)
+        config.camera_extrinsics = pinhole_params[min(valid_indices)].extrinsics.squeeze().numpy(force=True)
+        config.input_frames = len(gt_images)
+        config.frame_provider = 'precomputed'
+        config.segmentation_provider = 'SAM2'
+
         print('Data loading took {:.2f} seconds'.format((time.time() - t0) / 1))
 
-        run_tracking_on_sequence(config, write_folder, gt_Se3_cam2obj=gt_Se3_cam_to_obj,
-                                 images_paths=gt_images, segmentation_paths=gt_segs,
-                                 initial_segmentation=first_segmentation, initial_image=first_image)
+        sfb = Tracker6D(config, write_folder, initial_gt_Se3_cam2obj=gt_Se3_obj2cam_frame0,
+                        gt_Se3_cam2obj=dict_gt_Se3_cam2obj, images_paths=gt_images, segmentation_paths=gt_segs,
+                        initial_segmentation=first_segmentation,
+                        initial_image=first_image)
+        sfb.run_filtering_with_reconstruction()
 
 
 if __name__ == "__main__":
