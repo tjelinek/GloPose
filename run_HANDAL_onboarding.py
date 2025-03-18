d315682fd990196c1c8cde4ed540680095c63489
tjelinek <tomasjelinek96@gmail.com>
2025-03-18 10:50:27 +0100

    handal onboarding sequence runnable

--- run_HANDAL_onboarding.py
+++ run_HANDAL_onboarding.py
@@ -2,11 +2,10 @@ import time
 from pathlib import Path
 
 from utils.data_utils import get_initial_image_and_segment
-from utils.bop_challenge import (get_pinhole_params, load_gt_images_and_segmentations,
-                                 read_obj2cam_Se3_from_gt)
+from utils.bop_challenge import get_pinhole_params, load_gt_images_and_segmentations, extract_gt_Se3_cam2obj
 from utils.general import load_config
 from utils.runtime_utils import parse_args
-from tracker6d import run_tracking_on_sequence
+from tracker6d import Tracker6D
 
 
 def main():
@@ -53,28 +52,33 @@ def main():
 
         pose_json_path = sequence_folder / 'scene_gt.json'
 
-        dict_gt_Se3_obj2cam = read_obj2cam_Se3_from_gt(pose_json_path, config.device)
-        obj_ids = sorted(dict_gt_Se3_obj2cam.keys())
-        dict_gt_Se3_obj2cam = dict_gt_Se3_obj2cam[obj_ids[0]]
-        gt_Se3_obj2cam_frame0 = dict_gt_Se3_obj2cam[0]
+        dict_gt_Se3_cam2obj = extract_gt_Se3_cam2obj(pose_json_path, None, config.device)
+        gt_Se3_obj2cam_frame0 = dict_gt_Se3_cam2obj[min(dict_gt_Se3_cam2obj.keys())]
 
-        valid_indices = valid_indices[::skip_indices]
+        valid_indices = sorted(list(dict_gt_Se3_cam2obj.keys()))[::skip_indices]
+        gt_images = [gt_images[i] for i in valid_indices]
+        gt_segs = [gt_segs[i] for i in valid_indices]
+        dict_gt_Se3_cam2obj = {i: dict_gt_Se3_cam2obj[frame] for i, frame in enumerate(valid_indices)}
 
         first_image, first_segmentation = get_initial_image_and_segment(gt_images, gt_segs, segmentation_channel=0)
 
         pinhole_params = get_pinhole_params(sequence_folder / 'scene_camera.json')
 
-        config.camera_intrinsics = pinhole_params[0].intrinsics.squeeze().numpy(force=True)
-        config.camera_extrinsics = pinhole_params[0].extrinsics.squeeze().numpy(force=True)
+        config.camera_intrinsics = pinhole_params[min(valid_indices)].intrinsics.squeeze().numpy(force=True)
+        config.camera_extrinsics = pinhole_params[min(valid_indices)].extrinsics.squeeze().numpy(force=True)
         config.input_frames = len(gt_images)
         config.frame_provider = 'precomputed'
         config.segmentation_provider = 'SAM2'
 
         print('Data loading took {:.2f} seconds'.format((time.time() - t0) / 1))
 
-        run_tracking_on_sequence(config, write_folder, initial_gt_Se3_cam2obj=gt_Se3_obj2cam_frame0,
-                                 images_paths=gt_images, segmentation_paths=gt_segs,
-                                 initial_segmentation=first_segmentation, initial_image=first_image)
+        sfb = Tracker6D(config, write_folder, initial_gt_Se3_cam2obj=gt_Se3_obj2cam_frame0,
+                        gt_Se3_cam2obj=dict_gt_Se3_cam2obj, images_paths=gt_images, segmentation_paths=gt_segs,
+                        initial_segmentation=first_segmentation,
+                        initial_image=first_image)
+        sfb.run_filtering_with_reconstruction()
+
+        exit()
 
 
 if __name__ == "__main__":
