026ba43952152ae5c49b76478e3cf09690aa23bd
tjelinek <tomasjelinek96@gmail.com>
2025-04-03 11:41:39 +0200

    added new config var, deleted old

--- tracker_config.py
+++ tracker_config.py
@@ -30,10 +30,10 @@ class TrackerConfig:
 
     # Input data settings
     input_frames: int = 0
+    skip_indices: int = 1
     frame_provider: str = 'synthetic'  # 'precomputed' or 'synthetic'
     segmentation_provider: str = 'SAM2'  # 'precomputed', 'SAM2', 'whites', or 'synthetic'
     gt_flow_source: str = 'FlowNetwork'  # One of 'FlowNetwork', 'GenerateSynthetic'
-    mode: str = 'object_tracking'  # One of 'object_tracking' or 'camera_tracking'
     image_downsample: float = 1.0
 
     # Renderer settings
