7b3709be3b1aeba2aa9fce5847aeb841fc1344b6
tjelinek <tomasjelinek96@gmail.com>
2025-05-21 16:02:11 +0200

    HO3D runnable on evaluation data

--- run_HO3D.py
+++ run_HO3D.py
@@ -13,18 +13,25 @@ from utils.general import load_config
 
 def main():
     dataset = 'HO3D'
+    split = 'evaluation'  # or 'train'
+
     args = parse_args()
     if args.sequences is not None and len(args.sequences) > 0:
         sequences = args.sequences
     else:
-        sequences = [
-            'ABF10', 'BB10', 'GPMF10', 'GSF10', 'MC1', 'MDF10', 'ND2', 'ShSu12', 'SiBF12', 'SM3', 'SMu41',
-            'ABF11', 'BB11', 'GPMF11', 'GSF11', 'MC2', 'MDF11', 'SB10', 'ShSu13', 'SiBF13', 'SM4', 'SMu42',
-            'ABF12', 'BB12', 'GPMF12', 'GSF12', 'MC4', 'MDF12', 'SB12', 'ShSu14', 'SiBF14', 'SM5', 'SS1',
-            'ABF13', 'BB13', 'GPMF13', 'GSF13', 'MC5', 'MDF13', 'SB14', 'SiBF10', 'SiS1', 'SMu1', 'SS2',
-            'ABF14', 'BB14', 'GPMF14', 'GSF14', 'MC6', 'MDF14', 'ShSu10', 'SiBF11', 'SM2', 'SMu40', 'SS3',
-
-        ]
+        if split == 'train':
+            sequences = [
+                'ABF10', 'BB10', 'GPMF10', 'GSF10', 'MC1', 'MDF10', 'ND2', 'ShSu12', 'SiBF12', 'SM3', 'SMu41',
+                'ABF11', 'BB11', 'GPMF11', 'GSF11', 'MC2', 'MDF11', 'SB10', 'ShSu13', 'SiBF13', 'SM4', 'SMu42',
+                'ABF12', 'BB12', 'GPMF12', 'GSF12', 'MC4', 'MDF12', 'SB12', 'ShSu14', 'SiBF14', 'SM5', 'SS1',
+                'ABF13', 'BB13', 'GPMF13', 'GSF13', 'MC5', 'MDF13', 'SB14', 'SiBF10', 'SiS1', 'SMu1', 'SS2',
+                'ABF14', 'BB14', 'GPMF14', 'GSF14', 'MC6', 'MDF14', 'ShSu10', 'SiBF11', 'SM2', 'SMu40', 'SS3',
+            ]
+        else:
+            sequences = [
+                'AP10',  'AP12',  'AP14',   'MPM11',  'MPM13',  'SB11',  'SM1',
+                'AP11',  'AP13',  'MPM10',  'MPM12',  'MPM14',  'SB13',
+            ]
 
     sequence = sequences[0]
     config = load_config(args.config)
@@ -47,9 +54,11 @@ def main():
     config.write_folder = write_folder
     t0 = time.time()
 
-    sequence_folder = config.default_data_folder / 'HO3D' / 'train' / sequence
+    sequence_folder = config.default_data_folder / 'HO3D' / split / sequence
     image_folder = sequence_folder / 'rgb'
     segmentation_folder = sequence_folder / 'seg'
+    if not segmentation_folder.exists():
+        segmentation_folder = sequence_folder / 'segmentation_rendered'
     depth_folder = sequence_folder / 'depth'
     meta_folder = sequence_folder / 'meta'
 
@@ -60,12 +69,23 @@ def main():
     gt_translations = []
     gt_rotations = []
     cam_intrinsics_list = []
+
+    nones = set()
     for i, file in enumerate(sorted(meta_folder.iterdir())):
         if file.suffix not in ['.npz', '.pkl']:
             continue
 
         data = np.load(file, allow_pickle=True)
         data_dict = {key: data[key] for key in data}
+
+        cam_K = data_dict['camMat']
+        gt_rot = data_dict['objRot']
+        gt_trans = data_dict['objTrans']
+
+        if cam_K is None or gt_rot is None or gt_trans is None:
+            nones.add(i)
+            continue
+
         cam_intrinsics_list.append(data_dict['camMat'])
         gt_rotations.append(data_dict['objRot'].squeeze())
         gt_translations.append(data_dict['objTrans'])
@@ -73,7 +93,8 @@ def main():
     eerr0 = set(i for i in range(len(gt_rotations)) if len(gt_rotations[i].shape) < 1)
     eert0 = set(i for i in range(len(gt_rotations)) if len(gt_translations[i].shape) < 1)
 
-    valid_indices = [i for i in range(len(gt_rotations)) if i not in (eerr0 | eert0)]
+    invalid_indices = eerr0 | eert0 | nones
+    valid_indices = [i for i in range(len(gt_rotations)) if i not in invalid_indices]
 
     # Filter the lists to include only valid elements
     filtered_gt_rotations = [gt_rotations[i] for i in valid_indices]
