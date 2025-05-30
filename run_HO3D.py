ac7bccd29aaddd6fc50ab148b611af1109117a15
tjelinek <tomasjelinek96@gmail.com>
2025-05-30 14:58:02 +0200

    less skip indices

--- run_HO3D.py
+++ run_HO3D.py
@@ -35,7 +35,7 @@ def main():
 
     sequence = sequences[0]
     config = load_config(args.config)
-    config.skip_indices = 15
+    config.skip_indices = 10
 
     if config.gt_flow_source == 'GenerateSynthetic':
         exit()
