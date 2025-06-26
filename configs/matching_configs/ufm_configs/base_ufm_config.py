5589ccbdcca84eaf85df8623c41b6f9ce8a97e29
tjelinek <tomasjelinek96@gmail.com>
2025-06-26 17:12:44 +0200

    added base_ufm_config.py

---
+++ configs/matching_configs/ufm_configs/base_ufm_config.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Tuple
+
+import numpy as np
+
+
+@dataclass
+class BaseUFMConfig:
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        self.config_name: str = self.__class__.__name__
+
+        self.use_custom_weights: bool = False
