1787a69d783e9472482fbe9f06d227c0892edb56
tjelinek <tomasjelinek96@gmail.com>
2025-07-02 18:30:36 +0200

    del general threshold cfgs, new threshold cfgs, shorter gpu runtime

---
+++ configs/matchability_thresholds/ufm_c05r01.py
@@ -0,0 +1,12 @@
+from tracker_config import TrackerConfig
+
+
+def get_config() -> TrackerConfig:
+    cfg = TrackerConfig()
+
+    cfg.min_roma_certainty_threshold = 0.5
+    cfg.flow_reliability_threshold = 0.1
+    cfg.frame_filter = 'dense_matching'
+    cfg.dense_matching = 'UFM''
+
+    return cfg
