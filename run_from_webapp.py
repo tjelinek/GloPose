dbf45b0024fcd3a0077822ec3d9d5bc3b80faa1c
tjelinek <tomasjelinek96@gmail.com>
2025-07-14 10:43:44 +0200

    copy-paste of run_HO3D.py

--- run_from_webapp.py
+++ run_from_webapp.py
@@ -1,118 +1,159 @@
 import numpy as np
 import torch
 import time
-import torchvision.transforms as transforms
 from pathlib import Path
 
-from PIL import Image
-from kornia.geometry import Quaternion, Se3
+from kornia.geometry import Quaternion, Se3, PinholeCamera
 
-from utils.runtime_utils import parse_args
-from tracker6d import run_tracking_on_sequence
+from utils.data_utils import get_initial_image_and_segment
+from utils.runtime_utils import parse_args, exception_logger
+from tracker6d import Tracker6D
 from utils.general import load_config
 
 
 def main():
     dataset = 'HO3D'
+    split = 'train'  # 'evaluation' or 'train'
+
+    train_sequences = [
+                'ABF10', 'BB10', 'GPMF10', 'GSF10', 'MC1', 'MDF10', 'ND2', 'ShSu12', 'SiBF12', 'SM3', 'SMu41',
+                'ABF11', 'BB11', 'GPMF11', 'GSF11', 'MC2', 'MDF11', 'SB10', 'ShSu13', 'SiBF13', 'SM4', 'SMu42',
+                'ABF12', 'BB12', 'GPMF12', 'GSF12', 'MC4', 'MDF12', 'SB12', 'ShSu14', 'SiBF14', 'SM5', 'SS1',
+                'ABF13', 'BB13', 'GPMF13', 'GSF13', 'MC5', 'MDF13', 'SB14', 'SiBF10', 'SiS1', 'SMu1', 'SS2',
+                'ABF14', 'BB14', 'GPMF14', 'GSF14', 'MC6', 'MDF14', 'ShSu10', 'SiBF11', 'SM2', 'SMu40', 'SS3',
+            ]
+    test_sequences = [
+                'AP10', 'AP12', 'AP14', 'MPM11', 'MPM13', 'SB11', 'SM1',
+                'AP11', 'AP13', 'MPM10', 'MPM12', 'MPM14', 'SB13',
+            ]
     args = parse_args()
     if args.sequences is not None and len(args.sequences) > 0:
         sequences = args.sequences
     else:
-        sequences = [
-            'ABF10',  'BB10',  'GPMF10',  'GSF10',  'MC1',  'MDF10',  'ND2',     'ShSu12',  'SiBF12',  'SM3',    'SMu41',
-            'ABF11',  'BB11',  'GPMF11',  'GSF11',  'MC2',  'MDF11',  'SB10',    'ShSu13',  'SiBF13',  'SM4',    'SMu42',
-            'ABF12',  'BB12',  'GPMF12',  'GSF12',  'MC4',  'MDF12',  'SB12',    'ShSu14',  'SiBF14',  'SM5',    'SS1',
-            'ABF13',  'BB13',  'GPMF13',  'GSF13',  'MC5',  'MDF13',  'SB14',    'SiBF10',  'SiS1',    'SMu1',   'SS2',
-            'ABF14',  'BB14',  'GPMF14',  'GSF14',  'MC6',  'MDF14',  'ShSu10',  'SiBF11',  'SM2',     'SMu40',  'SS3',
-
-        ]
-
-    skip_indices = 15
+        if split == 'train':
+            sequences = train_sequences[0:1]
+        else:
+            sequences = test_sequences[3:4]
 
     for sequence in sequences:
-        config = load_config(args.config)
 
-        experiment_name = args.experiment
+        if args.sequences is not None and len(args.sequences) > 0:
+            if sequence in train_sequences:
+                split = 'train'
+            else:
+                split = 'evaluation'
 
-        config.experiment_name = experiment_name
-        config.sequence = sequence
-        config.dataset = dataset
-        config.image_downsample = 1.0
+        with exception_logger():
+            config = load_config(args.config)
 
-        if args.output_folder is not None:
-            write_folder = Path(args.output_folder) / dataset / sequence
-        else:
-            write_folder = config.default_results_folder / experiment_name / dataset / sequence
+            config.skip_indices *= 10
 
-        config.write_folder = write_folder
-        t0 = time.time()
+            if config.gt_flow_source == 'GenerateSynthetic':
+                exit()
 
-        sequence_folder = config.default_data_folder / 'HO3D' / 'train' / sequence
-        image_folder = sequence_folder / 'rgb'
-        segmentation_folder = sequence_folder / 'seg'
-        meta_folder = sequence_folder / 'meta'
+            experiment_name = args.experiment
+            config.experiment_name = experiment_name
+            config.sequence = sequence
+            config.dataset = dataset
+            config.image_downsample = 1.0
+            config.frame_provider_config.erode_segmentation = True
 
-        gt_images_list = [file for file in sorted(image_folder.iterdir()) if file.is_file()]
-        gt_segmentations_list = [file for file in sorted(segmentation_folder.iterdir()) if file.is_file()]
+            if args.output_folder is not None:
+                write_folder = Path(args.output_folder) / dataset / sequence
+            else:
+                write_folder = config.default_results_folder / experiment_name / dataset / sequence
 
-        gt_translations = []
-        gt_rotations = []
-        cam_intrinsics_list = []
-        for i, file in enumerate(sorted(meta_folder.iterdir())):
-            if file.suffix != '.npz':
-                continue
+            config.write_folder = write_folder
+            t0 = time.time()
 
-            data = np.load(file, allow_pickle=True)
-            data_dict = {key: data[key] for key in data}
-            cam_intrinsics_list.append(data_dict['camMat'])
-            gt_rotations.append(data_dict['objRot'].squeeze())
-            gt_translations.append(data_dict['objTrans'])
+            sequence_folder = config.default_data_folder / 'HO3D' / split / sequence
+            image_folder = sequence_folder / 'rgb'
+            segmentation_folder = sequence_folder / 'seg'
+            if not segmentation_folder.exists():
+                segmentation_folder = sequence_folder / 'segmentation_rendered'
+            depth_folder = sequence_folder / 'depth'
+            meta_folder = sequence_folder / 'meta'
 
-        eerr0 = set(i for i in range(len(gt_rotations)) if len(gt_rotations[i].shape) < 1)
-        eert0 = set(i for i in range(len(gt_rotations)) if len(gt_translations[i].shape) < 1)
+            gt_images_list = [file for file in sorted(image_folder.iterdir()) if file.is_file()]
+            gt_segmentations_list = [file for file in sorted(segmentation_folder.iterdir()) if file.is_file()]
+            gt_depths_list = [file for file in sorted(depth_folder.iterdir()) if file.is_file()]
 
-        valid_indices = [i for i in range(len(gt_rotations)) if i not in (eerr0 | eert0)]
-        valid_indices = valid_indices[::skip_indices]
+            gt_translations = []
+            gt_rotations = []
+            cam_intrinsics_list = []
 
-        # Filter the lists to include only valid elements
-        filtered_gt_rotations = [gt_rotations[i] for i in valid_indices]
-        filtered_gt_translations = [gt_translations[i] for i in valid_indices]
-        gt_images_list = [gt_images_list[i] for i in valid_indices]
-        gt_segmentations_list = [gt_segmentations_list[i] for i in valid_indices]
+            nones = set()
+            for i, file in enumerate(sorted(meta_folder.iterdir())):
+                if file.suffix not in ['.pkl']:
+                    continue
 
-        config.input_frames = len(gt_images_list)
+                data = np.load(file, allow_pickle=True)
+                data_dict = {key: data[key] for key in data}
 
-        rotations_array = torch.from_numpy(np.array(filtered_gt_rotations)).cuda()
-        translations_array = torch.from_numpy(np.array(filtered_gt_translations)).cuda()
+                cam_K = data_dict['camMat']
+                gt_rot = data_dict['objRot']
+                gt_trans = data_dict['objTrans']
 
-        # config.rot_init = tuple(rotations_array[0, 0].numpy(force=True).tolist())
-        # config.tran_init = tuple(translations_array[0, 0, 0].numpy(force=True).tolist())
-        # config.camera_up = (0, 0, 1)
+                if cam_K is None or gt_rot is None or gt_trans is None:
+                    nones.add(i)
+                    continue
 
-        print('Data loading took {:.2f} seconds'.format((time.time() - t0) / 1))
+                cam_intrinsics_list.append(data_dict['camMat'])
+                gt_rotations.append(data_dict['objRot'].squeeze())
+                gt_translations.append(data_dict['objTrans'])
 
-        quat_frame1 = Quaternion.from_axis_angle(torch.from_numpy(gt_rotations[0])[None])
-        trans_frame1 = torch.from_numpy(gt_translations[0])[None]
-        Se3_cam_to_obj = Se3(quat_frame1, trans_frame1)
-        T_cam_to_obj = Se3_cam_to_obj.matrix().squeeze()
+            eerr0 = set(i for i in range(len(gt_rotations)) if len(gt_rotations[i].shape) < 1)
+            eert0 = set(i for i in range(len(gt_rotations)) if len(gt_translations[i].shape) < 1)
 
-        config.camera_intrinsics = cam_intrinsics_list[0]
-        config.camera_extrinsics = T_cam_to_obj.numpy(force=True)
+            invalid_indices = eerr0 | eert0 | nones
+            valid_indices = [i for i in range(len(gt_rotations)) if i not in invalid_indices]
 
-        config.segmentation_provider = 'SAM2'
-        config.frame_provider = 'precomputed'
+            # Filter the lists to include only valid elements
+            filtered_gt_rotations = [gt_rotations[i] for i in valid_indices]
+            filtered_gt_translations = [gt_translations[i] for i in valid_indices]
+            gt_images_list = [gt_images_list[i] for i in valid_indices]
+            gt_segmentations_list = [gt_segmentations_list[i] for i in valid_indices]
 
-        first_segment = Image.open(gt_segmentations_list[0])
-        first_image = Image.open(gt_images_list[0])
+            config.input_frames = len(gt_images_list)
 
-        first_segment_resized = first_segment.resize(first_image.size, Image.NEAREST)
+            cam2obj_rotations = torch.from_numpy(np.array(filtered_gt_rotations)).to(config.device)
+            cam2obj_translations = torch.from_numpy(np.array(filtered_gt_translations)).to(config.device)
+            cam2obj_translations *= 1000.0  # Scaling from mm to m
 
-        transform = transforms.ToTensor()
-        first_segment_tensor = transform(first_segment_resized)[1].squeeze()  # Green channel is the obj segmentation
-        first_image_tensor = transform(first_image).squeeze()
+            Se3_cam2obj = Se3(Quaternion.from_axis_angle(cam2obj_rotations), cam2obj_translations)
+            Se3_obj2cam = Se3_cam2obj.inverse()
+            Se3_cam2obj_dict = {i: Se3_cam2obj[i] for i in range(config.input_frames)}
+            Se3_obj2cam_dict = {i: Se3_obj2cam[i] for i in range(config.input_frames)}
 
-        run_tracking_on_sequence(config, write_folder, gt_texture=None, gt_mesh=None, gt_rotations=rotations_array,
-                                 gt_translations=translations_array, images_paths=gt_images_list,
-                                 segmentation_paths=gt_segmentations_list, initial_segmentation=first_segment_tensor,
-                                 initial_image=first_image_tensor)
+            print('Data loading took {:.2f} seconds'.format((time.time() - t0) / 1))
 
+            T_obj_to_cam = Se3_cam2obj.inverse().matrix().squeeze()
+
+            config.camera_intrinsics = cam_intrinsics_list[0]
+            config.camera_extrinsics = T_obj_to_cam.numpy(force=True)
+            config.large_images_results_write_frequency = 8
+
+            config.segmentation_provider = 'SAM2'
+            config.frame_provider = 'precomputed'
+
+            first_image_tensor, first_segment_tensor = get_initial_image_and_segment(gt_images_list, gt_segmentations_list,
+                                                                                     segmentation_channel=1)
+
+            image_h, image_w = (torch.tensor(int(s * config.image_downsample))[None].to(config.device)
+                                for s in first_image_tensor.shape[-2:])
+
+            gt_pinhole_params = {}
+            for i in range(config.input_frames):
+                obj2cam = Se3_obj2cam_dict[i].matrix()[None]
+                cam_K_tensor = torch.from_numpy(cam_intrinsics_list[i])[None].to(config.device)
+                gt_pinhole_params[i] = PinholeCamera(cam_K_tensor, obj2cam, image_h, image_w)
+
+            sfb = Tracker6D(config, write_folder, images_paths=gt_images_list, gt_Se3_cam2obj=Se3_cam2obj_dict,
+                            gt_Se3_world2cam=Se3_obj2cam_dict, segmentation_paths=gt_segmentations_list,
+                            initial_image=first_image_tensor, initial_segmentation=first_segment_tensor,
+                            depth_paths=gt_depths_list, gt_pinhole_params=gt_pinhole_params)
+            sfb.run_pipeline()
+
+
+if __name__ == "__main__":
+    main()
