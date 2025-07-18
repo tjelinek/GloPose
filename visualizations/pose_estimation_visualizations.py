00fa8a43521bba41f466c1be979ef5b93a81d725
tjelinek <tomasjelinek96@gmail.com>
2025-07-18 11:04:47 +0200

    hopefully solved all the missing imports

--- visualizations/pose_estimation_visualizations.py
+++ visualizations/pose_estimation_visualizations.py
@@ -3,44 +3,29 @@ import rerun as rr
 import torch
 import torchvision.transforms.functional as TF
 from matplotlib import pyplot as plt
+from kornia.image import ImageSize
 
 from data_structures.rerun_annotations import RerunAnnotations
 from tracker_config import TrackerConfig
 from utils.image_utils import overlay_mask
+from utils.results_logging import log_correspondences_rerun
 
 
-def visualize_flow_with_matching_rerun(frame_i: int, config: TrackerConfig):
+def visualize_flow_with_matching_rerun(frame_i: int, src_pts_xy: torch.Tensor, dst_pts_xy: torch.Tensor,
+                                       certainty: torch.Tensor, config: TrackerConfig,
+                                       viewgraph_image: torch.Tensor, query_image: torch.Tensor, reliability: float,
+                                       reliability_threshold: float, certainty_threshold,
+                                       match_certainty_map: torch.Tensor = None,):
 
-    datagraph_camera_data = self.data_graph.get_frame_data(frame_i)
-    new_flow_arc = (datagraph_camera_data.matching_source_keyframe, frame_i)
-    flow_arc_source, flow_arc_target = new_flow_arc
+    template_image = viewgraph_image
+    target_image = query_image
 
-    arc_observation = self.data_graph.get_edge_observations(flow_arc_source, flow_arc_target)
-
-    template_data = self.data_graph.get_frame_data(flow_arc_source)
-    target_data = self.data_graph.get_frame_data(flow_arc_target)
-
-    if config.frame_filter == 'dense_matching':
-        reliability = arc_observation.reliability_score
-        rr.log(RerunAnnotations.matching_reliability, rr.Scalar(reliability))
-        rr.log(RerunAnnotations.matching_reliability_threshold_roma,
-               rr.Scalar(target_data.current_flow_reliability_threshold))
-        if config.matchability_based_reliability:
-            matchability_share = template_data.relative_area_matchable
-            min_roma_certainty = template_data.roma_certainty_threshold
-            rr.log(RerunAnnotations.matching_matchability_plot_share_matchable, rr.Scalar(matchability_share))
-            rr.log(RerunAnnotations.matching_min_roma_certainty_plot_min_certainty, rr.Scalar(min_roma_certainty))
-    elif config.frame_filter == 'SIFT':
-        rr.log(RerunAnnotations.matches_sift, rr.Scalar(arc_observation.num_matches))
-        rr.log(RerunAnnotations.min_matches_sift, rr.Scalar(config.sift_filter_min_matches))
-        rr.log(RerunAnnotations.good_to_add_number_of_matches_sift,
-               rr.Scalar(config.sift_filter_good_to_add_matches))
+    rr.log(RerunAnnotations.matching_reliability, rr.Scalar(reliability))
+    rr.log(RerunAnnotations.matching_reliability_threshold_roma,
+           rr.Scalar(reliability_threshold))
 
     if frame_i == 0 or (frame_i % config.large_images_results_write_frequency != 0):
         return
-
-    template_image = template_data.frame_observation.observed_image.squeeze()
-    target_image = target_data.frame_observation.observed_image.squeeze()
 
     template_target_image = torch.cat([template_image, target_image], dim=-2)
     template_target_image_np = template_target_image.permute(1, 2, 0).numpy(force=True)
@@ -48,87 +33,36 @@ def visualize_flow_with_matching_rerun(frame_i: int, config: TrackerConfig):
     rr.log(RerunAnnotations.matches_high_certainty, rerun_image)
     rr.log(RerunAnnotations.matches_low_certainty, rerun_image)
 
-    if config.frame_filter == 'dense_matching':
-        certainties = arc_observation.src_dst_certainty_roma.numpy(force=True)
-        threshold = template_data.roma_certainty_threshold
-        if threshold is None:
-            threshold = config.min_roma_certainty_threshold
+    certainties = certainty.numpy(force=True)
+    threshold = certainty_threshold
+    if threshold is None:
+        threshold = config.min_roma_certainty_threshold
 
-        above_threshold_mask = certainties >= threshold
-        src_pts_xy_roma = arc_observation.src_pts_xy_roma[:, [1, 0]].numpy(force=True)
-        dst_pts_xy_roma = arc_observation.dst_pts_xy_roma[:, [1, 0]].numpy(force=True)
+    above_threshold_mask = certainties >= threshold
+    src_pts_xy_roma = src_pts_xy[:, [1, 0]].numpy(force=True)
+    dst_pts_xy_roma = dst_pts_xy[:, [1, 0]].numpy(force=True)
 
-        inliers_source_yx = src_pts_xy_roma[above_threshold_mask]
-        inliers_target_yx = dst_pts_xy_roma[above_threshold_mask]
-        outliers_source_yx = src_pts_xy_roma[~above_threshold_mask]
-        outliers_target_yx = dst_pts_xy_roma[~above_threshold_mask]
+    inliers_source_yx = src_pts_xy_roma[above_threshold_mask]
+    inliers_target_yx = dst_pts_xy_roma[above_threshold_mask]
+    outliers_source_yx = src_pts_xy_roma[~above_threshold_mask]
+    outliers_target_yx = dst_pts_xy_roma[~above_threshold_mask]
 
-        if config.matchability_based_reliability:
-            certainties_matchable = arc_observation.src_dst_certainty_roma_matchable.numpy(force=True)
-            above_threshold_mask_matchable = certainties_matchable >= threshold
-            src_pts_xy_roma_matchable = arc_observation.src_pts_xy_roma_matchable[:, [1, 0]].numpy(force=True)
-            dst_pts_xy_roma_matchable = arc_observation.dst_pts_xy_roma_matchable[:, [1, 0]].numpy(force=True)
-            inliers_source_yx_matchable = src_pts_xy_roma_matchable[above_threshold_mask_matchable]
-            inliers_target_yx_matchable = dst_pts_xy_roma_matchable[above_threshold_mask_matchable]
-            outliers_source_yx_matchable = src_pts_xy_roma_matchable[~above_threshold_mask_matchable]
-            outliers_target_yx_matchable = dst_pts_xy_roma_matchable[~above_threshold_mask_matchable]
+    roma_certainty_map = match_certainty_map if match_certainty_map is not None else torch.ones_like(template_target_image)
+    roma_h, roma_w = roma_certainty_map.shape[0], roma_certainty_map.shape[1] // 2
+    certainty_map_column = torch.zeros(roma_h * 2, roma_w).to(roma_certainty_map.device)
+    certainty_map_column[:roma_h, :roma_w] = roma_certainty_map[:roma_h, :roma_w]
+    certainty_map_column[roma_h:, :roma_w] = roma_certainty_map[:roma_h, roma_w:]
+    certainty_map_column = certainty_map_column[None]
+    roma_certainty_map_image_size = TF.resize(certainty_map_column, size=template_target_image.shape[1:])
 
-        roma_certainty_map = arc_observation.roma_flow_warp_certainty
-        roma_h, roma_w = roma_certainty_map.shape[0], roma_certainty_map.shape[1] // 2
-        certainty_map_column = torch.zeros(roma_h * 2, roma_w).to(roma_certainty_map.device)
-        certainty_map_column[:roma_h, :roma_w] = roma_certainty_map[:roma_h, :roma_w]
-        certainty_map_column[roma_h:, :roma_w] = roma_certainty_map[:roma_h, roma_w:]
-        certainty_map_column = certainty_map_column[None]
-        roma_certainty_map_image_size = TF.resize(certainty_map_column, size=template_target_image.shape[1:])
+    roma_certainty_map_im_size_np = roma_certainty_map_image_size.numpy(force=True)
+    template_target_blacks = np.ones_like(template_target_image_np)
+    template_target_image_certainty_np = overlay_mask(template_target_blacks, roma_certainty_map_im_size_np)
 
-        roma_certainty_map_im_size_np = roma_certainty_map_image_size.numpy(force=True)
-        template_target_blacks = np.ones_like(template_target_image_np)
-        template_target_image_certainty_np = overlay_mask(template_target_blacks, roma_certainty_map_im_size_np)
+    rerun_certainty_img = rr.Image(template_target_image_certainty_np)
+    rr.log(RerunAnnotations.matching_certainty, rerun_certainty_img)
 
-        rerun_certainty_img = rr.Image(template_target_image_certainty_np)
-        rr.log(RerunAnnotations.matching_certainty, rerun_certainty_img)
-    elif config.frame_filter == 'SIFT':
-        keypoints_matching_indices = arc_observation.sift_keypoint_indices
-
-        template_src_pts = template_data.sift_keypoints
-        target_src_pts = target_data.sift_keypoints
-
-        inliers_source_xy = template_src_pts[keypoints_matching_indices[:, 0]]
-        inliers_target_xy = target_src_pts[keypoints_matching_indices[:, 1]]
-
-        inliers_source_yx = inliers_source_xy[:, [1, 0]].numpy(force=True)
-        inliers_target_yx = inliers_target_xy[:, [1, 0]].numpy(force=True)
-        outliers_source_yx = np.zeros((0, 2))
-        outliers_target_yx = np.zeros((0, 2))
-    else:
-        return
-
-    def log_correspondences_rerun(cmap, src_yx, target_yx, rerun_annotation, source_image_height, sample_size=None):
-        if sample_size is not None:
-            random_indices = torch.randperm(min(sample_size, src_yx.shape[0]))
-            src_yx = src_yx[random_indices]
-            target_yx = target_yx[random_indices]
-
-        if len(src_yx.shape) == 1 or src_yx.shape[1] == 0:
-            return  # No matches to draw
-        target_yx_2nd_image = target_yx
-        target_yx_2nd_image[:, 0] = source_image_height + target_yx_2nd_image[:, 0]
-
-        line_strips_xy = np.stack([src_yx[:, [1, 0]], target_yx_2nd_image[:, [1, 0]]], axis=1)
-
-        num_points = line_strips_xy.shape[0]
-        colors = [cmap(i / num_points)[:3] for i in range(num_points)]
-        colors = (np.array(colors) * 255).astype(int).tolist()
-
-        rr.log(
-            rerun_annotation,
-            rr.LineStrips2D(
-                strips=line_strips_xy,
-                colors=colors,
-            ),
-        )
-
-    template_image_size = template_data.image_shape
+    template_image_size = ImageSize(template_image.shape[-2:])
     cmap_inliers = plt.get_cmap('Greens')
     log_correspondences_rerun(cmap_inliers, inliers_source_yx, inliers_target_yx,
                               RerunAnnotations.matches_high_certainty, template_image_size.height, 20)
