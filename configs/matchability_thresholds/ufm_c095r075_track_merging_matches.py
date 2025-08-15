b51a407c2f9311c15f88adfb46b73cdd6f7086f7
tjelinek <tomasjelinek96@gmail.com>
2025-08-15 17:07:28 +0200

    new configs

---
+++ configs/matchability_thresholds/ufm_c095r075_track_merging_matches.py
@@ -0,0 +1,13 @@
+from tracker_config import TrackerConfig
+
+
+def get_config() -> TrackerConfig:
+    cfg = TrackerConfig()
+
+    cfg.min_roma_certainty_threshold = 0.95
+    cfg.flow_reliability_threshold = 0.75
+    cfg.frame_filter = 'dense_matching'
+    cfg.dense_matching = 'UFM'
+    cfg.add_track_merging_matches = True
+
+    return cfg
