ab660f6ec3b58c57b41429d092e527c8f8d907f7
tjelinek <tomasjelinek96@gmail.com>
2025-08-25 16:20:23 +0200

    merging of reconstructions hello world

---
+++ merge_static_onboarding.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+def merge_reconstructions_for_experiment(experiment_name: str):
+
+    base_folder = Path('/mnt/personal/jelint19/cache/view_graph_cache') / experiment_name
+
+
+def __init__():
+
+    experiment_name = 'ufm_c095r05_viewgraph_from_matching'
