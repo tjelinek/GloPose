df68d1f52e608df5e1b0d03520f1f03a1b09290c
tjelinek <tomasjelinek96@gmail.com>
2025-09-28 00:34:10 +0200

    argument for detector exposed to the scripts

--- scripts/precompute_cnos_detections.sh
+++ scripts/precompute_cnos_detections.sh
@@ -16,6 +16,19 @@ datasets=(
   hope-val
 )
 
+# Parse optional detector argument
+detector=""
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --detector)
+      detector="${2:-}"; shift 2 ;;
+    --detector=*)
+      detector="${1#*=}"; shift ;;
+    *)
+      echo "Unknown argument: $1" >&2; shift ;;
+  esac
+done
+
 # Decide which sbatch path to use
 if [[ $(basename "$PWD") == "scripts" ]]; then
   sbatch_cmd="sbatch precompute_cnos_detections_job.batch"
@@ -25,5 +38,14 @@ fi
 
 for ds in "${datasets[@]}"; do
   echo "Submitting job for dataset: $ds"
-  $sbatch_cmd --dataset "$ds"
+
+  # Build command with dataset
+  cmd_args="--dataset $ds"
+
+  # Add detector if specified
+  if [[ -n "${detector}" ]]; then
+    cmd_args="$cmd_args --detector $detector"
+  fi
+
+  $sbatch_cmd $cmd_args
 done
