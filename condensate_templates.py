c1aeb45afa24f6c1c0e88a6f36a3e31711e67fb6
tjelinek <tomasjelinek96@gmail.com>
2025-10-07 00:15:04 +0200

    have to clone the tensor so that it is not massive when saved

--- condensate_templates.py
+++ condensate_templates.py
@@ -373,7 +373,7 @@ def perform_condensation_per_dataset(bop_base: Path, cache_base_path: Path, data
         shutil.copy2(image_path, images_save_dir / new_image_name)
         shutil.copy2(segmentation_path, segmentation_save_dir / new_seg_name)
 
-        cls_descriptor_to_save = dino_cls_descriptors[index].detach().cpu()
+        cls_descriptor_to_save = dino_cls_descriptors[index].detach().cpu().clone()
         torch.save(cls_descriptor_to_save, descriptors_save_dir / descriptor_name)
         saved_indices.append(int(index))
         saved_labels.append(object_id)
