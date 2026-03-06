5870898ab79c99948f0c43ddac3bca303bd0637c
tjelinek <tomasjelinek96@gmail.com>
2026-03-06 12:15:33 +0100

    Add reconstruction configs for `mast3r` and `vggt` with every 8th frame using various background options

---
+++ configs/reconstruction/mast3r_every_8th_original_bg.py
@@ -0,0 +1,12 @@
+from configs.glopose_config import GloPoseConfig
+
+
+def get_config() -> GloPoseConfig:
+    cfg = GloPoseConfig()
+
+    cfg.onboarding.frame_filter = 'passthrough'
+    cfg.onboarding.allow_disk_cache = False
+    cfg.onboarding.passthrough_skip = 8
+    cfg.onboarding.reconstruction_method = 'mast3r'
+
+    return cfg
